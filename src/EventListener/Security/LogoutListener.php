c3841997043ca978973a1a1ebcca9bc5217bac5e
Florent8 <florent.martin1997@orange.fr>
2021-10-13 13:20:34 +0200

    fix: Capture des kernel.exception

--- src/EventListener/Security/LogoutListener.php
+++ src/EventListener/Security/LogoutListener.php
@@ -2,12 +2,11 @@
 
 namespace App\EventListener\Security;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 final class LogoutListener {
     public function __invoke(LogoutEvent $event): void {
-        $event->setResponse(new JsonResponse(null, Response::HTTP_NO_CONTENT));
+        $event->setResponse(new Response(null, Response::HTTP_NO_CONTENT));
     }
 }
