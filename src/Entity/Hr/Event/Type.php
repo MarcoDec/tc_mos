67d4b47b2b423c575a0485de99c8d23c371b67f5
firasmlika <firasmlika95@gmail.com>
2021-10-22 14:11:09 +0100

    ajout EventType

--- src/Entity/Hr/Event/Type.php
+++ src/Entity/Hr/Event/Type.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[
+#[ 
     
     ApiFilter(filterClass: SearchFilter::class, properties: [ 'name' => 'partial', 'toStatus' => 'partial']), 
     ApiResource(
