8f8e5485ee1f9ad4f937e6d3fc8f6ddc3f6a17ca
Florent8 <florent.martin1997@orange.fr>
2021-11-03 09:52:45 +0100

    Fixtures : Pays

--- dev/Fixtures/PropertyConfig.php
+++ dev/Fixtures/PropertyConfig.php
@@ -3,6 +3,7 @@
 namespace App\Fixtures;
 
 final class PropertyConfig {
+    private bool $country;
     private ?string $forceValue;
     private bool $new;
     private string $newName;
@@ -12,6 +13,7 @@ final class PropertyConfig {
      * @param array{force_value?: string, new?: bool, new_name: string, new_ref?: class-string, old_ref?: string} $config
      */
     public function __construct(private string $name, array $config) {
+        $this->country = $config['country'] ?? false;
         $this->forceValue = $config['force_value'] ?? null;
         $this->new = $config['new'] ?? false;
         $this->newName = $config['new_name'];
@@ -28,6 +30,10 @@ final class PropertyConfig {
 
     public function getOldRef(): ?string {
         return $this->oldRef;
+    }
+
+    public function isCountry(): bool {
+        return $this->country;
     }
 
     public function isNew(): bool {
