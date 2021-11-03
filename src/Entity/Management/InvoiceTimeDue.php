ebae812e46717c63c7ac9f1dee54ee1b75112861
Florent8 <florent.martin1997@orange.fr>
2021-11-03 11:41:43 +0100

    Reprise InvoiceTimeDue

--- src/Entity/Management/InvoiceTimeDue.php
+++ src/Entity/Management/InvoiceTimeDue.php
@@ -2,12 +2,13 @@
 
 namespace App\Entity\Management;
 
-use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Entity\Embeddable\Hr\Employee\Roles;
 use App\Entity\Entity;
+use App\Entity\Traits\NameTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -15,138 +16,109 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[
     ApiFilter(filterClass: SearchFilter::class, properties: ['days' => 'partial', 'daysAfterEndOfMonth' => 'partial', 'endOfMonth' => 'exact', 'name' => 'partial']),
     ApiResource(
-        description: 'InvoiceTimeDue',
+        description: 'Délai de paiement des factures',
         collectionOperations: [
             'get' => [
                 'openapi_context' => [
-                    'description' => 'Récupère les délais de facturation',
-                    'summary' => 'Récupère les délais de facturation',
+                    'description' => 'Récupère les délais de paiement des factures',
+                    'summary' => 'Récupère les délais de paiement des factures',
                 ]
             ],
             'post' => [
                 'openapi_context' => [
-                    'description' => 'Créer un délai de facturation',
-                    'summary' => 'Créer un délai de facturation',
+                    'description' => 'Créer un délai de paiement des factures',
+                    'summary' => 'Créer un délai de paiement des factures',
                 ]
             ]
         ],
         itemOperations: [
             'delete' => [
                 'openapi_context' => [
-                    'description' => 'Supprime un délai de facturation',
-                    'summary' => 'Supprime un délai de facturation',
+                    'description' => 'Supprime un délai de paiement des factures',
+                    'summary' => 'Supprime un délai de paiement des factures',
                 ]
             ],
             'get' => NO_ITEM_GET_OPERATION,
             'patch' => [
                 'openapi_context' => [
-                    'description' => 'Modifie un délai de facturation',
-                    'summary' => 'Modifie un délai de facturation',
+                    'description' => 'Modifie un délai de paiement des factures',
+                    'summary' => 'Modifie un délai de paiement des factures',
                 ]
             ]
         ],
-        shortName: 'InvoiceTimeDue',
         attributes: [
             'security' => 'is_granted(\''.Roles::ROLE_MANAGEMENT_ADMIN.'\')'
         ],
         denormalizationContext: [
-            'groups' => ['write:invoicetimedue', 'write:name'],
+            'groups' => ['write:invoice-time-due', 'write:name'],
             'openapi_definition_name' => 'InvoiceTimeDue-write'
         ],
         normalizationContext: [
-            'groups' => ['read:invoicetimedue', 'read:id', 'read:name'],
+            'groups' => ['read:invoice-time-due', 'read:id', 'read:name'],
             'openapi_definition_name' => 'InvoiceTimeDue-read'
-        ],
-        paginationEnabled: false
+        ]
     ),
-    ORM\Entity(),
-    ORM\Table(name: 'InvoiceTimeDue')
+    ORM\Entity,
+    ORM\Table
 ]
 class InvoiceTimeDue extends Entity {
+    use NameTrait;
+
     #[
-        ApiProperty(description: 'jours ', example: '60'),
-        ORM\Column( type:"smallint", options: ['default' => 0 ,'unsigned'=> true]),
-        Serializer\Groups(['read:invoicetimedue', 'write:invoicetimedue'])
+        ApiProperty(description: 'Nom', required: true, example: '30 jours fin de mois'),
+        ORM\Column,
+        Serializer\Groups(['read:name', 'write:name']),
+        Assert\NotBlank
+    ]
+    protected ?string $name = null;
+
+    #[
+        ApiProperty(description: 'Jours ', example: 30),
+        ORM\Column(type: 'smallint', options: ['default' => 0, 'unsigned' => true]),
+        Serializer\Groups(['read:invoice-time-due', 'write:invoice-time-due'])
 
     ]
     private ?int $days = 0;
 
     #[
-        ApiProperty(description: 'joursAprèsFinMois ', example: '0'),
-        ORM\Column( type:"smallint", options: ['default' => 0 ,'unsigned'=> true]),
-        Serializer\Groups(['read:invoicetimedue', 'write:invoicetimedue'])
+        ApiProperty(description: 'Jours après la fin du mois ', example: 0),
+        ORM\Column(type: 'smallint', options: ['default' => 0, 'unsigned' => true]),
+        Serializer\Groups(['read:invoice-time-due', 'write:invoice-time-due'])
 
     ]
     private ?int $daysAfterEndOfMonth = 0;
 
     #[
-        ApiProperty(description: 'fin du mois ', example: 'True'),
+        ApiProperty(description: 'Fin du mois ', example: true),
         ORM\Column(options: ['default' => false]),
-        Serializer\Groups(['read:invoicetimedue', 'write:invoicetimedue'])
+        Serializer\Groups(['read:invoice-time-due', 'write:invoice-time-due'])
     ]
     private ?bool $endOfMonth = false;
-   
-    #[
-        ORM\Column,
-        Serializer\Groups(['read:name', 'write:name']),
-        Assert\NotBlank
-        ]
-    private ?string $name;
 
-  
-    
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    
-    public function setName(?string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    
-    public function getDays(): ?int
-    {
+    final public function getDays(): ?int {
         return $this->days;
     }
 
-    
-    public function setDays(?int $days): self
-    {
-        $this->days = $days;
-
-        return $this;
-    }
-
-    
-    public function getDaysAfterEndOfMonth(): ?int
-    {
+    final public function getDaysAfterEndOfMonth(): ?int {
         return $this->daysAfterEndOfMonth;
     }
 
-    
-    public function setDaysAfterEndOfMonth(?int $daysAfterEndOfMonth): self
-    {
-        $this->daysAfterEndOfMonth = $daysAfterEndOfMonth;
-
-        return $this;
-    }
-
-    
-    public function getEndOfMonth(): ?bool
-    {
+    final public function getEndOfMonth(): ?bool {
         return $this->endOfMonth;
     }
 
-   
-    public function setEndOfMonth(?bool $endOfMonth): self
-    {
-        $this->endOfMonth = $endOfMonth;
+    final public function setDays(?int $days): self {
+        $this->days = $days;
+        return $this;
+    }
 
+    final public function setDaysAfterEndOfMonth(?int $daysAfterEndOfMonth): self {
+        $this->daysAfterEndOfMonth = $daysAfterEndOfMonth;
+        return $this;
+    }
+
+    final public function setEndOfMonth(?bool $endOfMonth): self {
+        $this->endOfMonth = $endOfMonth;
         return $this;
     }
 }
