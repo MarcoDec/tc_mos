bcc20c26ca113d40d78fdb65436a8def5f3f655b
Florent8 <florent.martin1997@orange.fr>
2021-11-05 11:42:30 +0100

    Reprise Carrier

--- src/Entity/Logistics/Carrier.php
+++ src/Entity/Logistics/Carrier.php
@@ -2,50 +2,50 @@
 
 namespace App\Entity\Logistics;
 
-use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Entity\Embeddable\Hr\Employee\Roles;
 use App\Entity\Entity;
+use App\Entity\Traits\NameTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[
-    ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial','address' => 'partial']),
+    ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial', 'address' => 'partial']),
     ApiResource(
-        description: 'Carrier',
+        description: 'Transporteur',
         collectionOperations: [
             'get' => [
                 'openapi_context' => [
-                    'description' => 'Récupère les Transporteurs',
-                    'summary' => 'Récupère les Transporteurs',
+                    'description' => 'Récupère les transporteurs',
+                    'summary' => 'Récupère les transporteurs',
                 ]
             ],
             'post' => [
                 'openapi_context' => [
-                    'description' => 'Créer un Transporteur',
-                    'summary' => 'Créer un Transporteur',
+                    'description' => 'Créer un transporteur',
+                    'summary' => 'Créer un transporteur',
                 ]
             ]
         ],
         itemOperations: [
             'delete' => [
                 'openapi_context' => [
-                    'description' => 'Supprime un Transporteur',
-                    'summary' => 'Supprime un Transporteur',
+                    'description' => 'Supprime un transporteur',
+                    'summary' => 'Supprime un transporteur',
                 ]
             ],
             'get' => NO_ITEM_GET_OPERATION,
             'patch' => [
                 'openapi_context' => [
-                    'description' => 'Modifie un Transporteur',
-                    'summary' => 'Modifie un Transporteur',
+                    'description' => 'Modifie un transporteur',
+                    'summary' => 'Modifie un transporteur',
                 ]
             ]
         ],
-        shortName: 'Carrier',
         attributes: [
             'security' => 'is_granted(\''.Roles::ROLE_LOGISTICS_ADMIN.'\')'
         ],
@@ -56,55 +56,35 @@ use Symfony\Component\Validator\Constraints as Assert;
         normalizationContext: [
             'groups' => ['read:carrier', 'read:id', 'read:name'],
             'openapi_definition_name' => 'OutTrainer-read'
-        ],
-        paginationEnabled: false
+        ]
     ),
-    ORM\Entity(),
-    ORM\Table(name: 'Carrier')
+    ORM\Entity,
+    ORM\Table
 ]
 class Carrier extends Entity {
-    #[
-        ApiProperty(description: 'nom', example: 'DHL'),
-        ORM\Column(nullable: true),
-        Serializer\Groups(['read:name', 'write:name']),
-        Assert\NotBlank
+    use NameTrait;
 
+    #[
+        ApiProperty(description: 'Nom', required: true, example: 'DHL'),
+        Assert\NotBlank,
+        ORM\Column,
+        Serializer\Groups(['read:name', 'write:name'])
     ]
-    private ?string $name = null;
+    protected ?string $name = null;
 
     #[
         ApiProperty(description: 'address', example: 'null'),
         ORM\Column(nullable: true),
         Serializer\Groups(['read:carrier', 'write:carrier'])
-        ]
+    ]
     private ?string $address = null;
 
-
-   
-    public function getAddress(): ?string
-    {
+    final public function getAddress(): ?string {
         return $this->address;
     }
 
-    
-    public function setAddress(?string $address): self
-    {
+    final public function setAddress(?string $address): self {
         $this->address = $address;
-
-        return $this;
-    }
-
-     
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-   
-    public function setName(?string $name): self
-    {
-        $this->name = $name;
-
         return $this;
     }
 }
