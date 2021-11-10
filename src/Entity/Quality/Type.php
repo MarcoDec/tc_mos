dc21011241af29cbbee44f75eaf7df3c26b6f144
Florent8 <florent.martin1997@orange.fr>
2021-11-10 15:14:30 +0100

    Reprise QualityType

--- src/Entity/Quality/Type.php
+++ src/Entity/Quality/Type.php
@@ -8,6 +8,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Entity\Embeddable\Hr\Employee\Roles;
 use App\Entity\Entity;
+use App\Entity\Traits\NameTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -15,67 +16,60 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[
     ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial']),
     ApiResource(
-        description: 'Quality',
+        description: 'Type qualité',
         collectionOperations: [
             'get' => [
                 'openapi_context' => [
-                    'description' => 'Récupère les qualités',
-                    'summary' => 'Récupère les qualités',
+                    'description' => 'Récupère les types qualités',
+                    'summary' => 'Récupère les types qualités',
                 ]
             ],
             'post' => [
                 'openapi_context' => [
-                    'description' => 'Créer une qualité',
-                    'summary' => 'Créer une qualité',
+                    'description' => 'Créer un type qualité',
+                    'summary' => 'Créer un type qualité',
                 ]
             ]
         ],
         itemOperations: [
             'delete' => [
                 'openapi_context' => [
-                    'description' => 'Supprime une qualité',
-                    'summary' => 'Supprime une qualité',
+                    'description' => 'Supprime un type qualité',
+                    'summary' => 'Supprime un type qualité',
                 ]
             ],
             'get' => NO_ITEM_GET_OPERATION,
             'patch' => [
                 'openapi_context' => [
-                    'description' => 'Modifie une qualité',
-                    'summary' => 'Modifie une qualité',
+                    'description' => 'Modifie un type qualité',
+                    'summary' => 'Modifie un type qualité',
                 ]
             ]
         ],
-        shortName: 'Qualite',
+        shortName: 'QualityType',
         attributes: [
             'security' => 'is_granted(\''.Roles::ROLE_QUALITY_ADMIN.'\')'
         ],
         denormalizationContext: [
-            'groups' => ['write:qualite', 'write:name'],
-            'openapi_definition_name' => 'Qualite-write'
+            'groups' => ['write:name'],
+            'openapi_definition_name' => 'QualityType-write'
         ],
         normalizationContext: [
-            'groups' => ['read:qualite', 'read:id', 'read:name'],
-            'openapi_definition_name' => 'Qualite-read'
+            'groups' => ['read:id', 'read:name'],
+            'openapi_definition_name' => 'QualityType-read'
         ],
-        paginationEnabled: false
     ),
-    ORM\Entity(),
+    ORM\Entity,
     ORM\Table(name: 'quality_type')
 ]
 class Type extends Entity {
+    use NameTrait;
+
     #[
         ApiProperty(description: 'Nom ', required: true, example: 'Dimensions'),
         Assert\NotBlank,
         ORM\Column,
         Serializer\Groups(['read:name', 'write:name'])
     ]
-    protected ?string $name;
-
-    public function getName(): ?string {
-        return $this->name;
-    }
-
-    public function setName(?string $name): void {
-        $this->name = $name;
-    }
+    protected ?string $name = null;
 }
