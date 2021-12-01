8ecbc4e97350b657df673456b622d889bef5583e
marcodec <marc.declercq@laposte.net>
2021-12-01 22:43:46 +0100

    Finalisation des vues debug-bar

--- src/DataCollector/CouchdbCollector.php
+++ src/DataCollector/CouchdbCollector.php
@@ -2,6 +2,7 @@
 
 namespace App\DataCollector;
 
+use App\Entity\Management\Notification;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
@@ -15,26 +16,53 @@ class CouchdbCollector extends DataCollector
     */
    public function collect(Request $request, Response $response, \Throwable $exception = null):void
    {
-      $this->data['title']= [
-         'value'=> 'green',
-         'status'=> 'STATUS',
-         'size'=> 'SIZE'
+      $this->data['actions']= [
+        [
+           "document"=> Notification::class,
+           "requestType" => "create",
+           "detail" => "{{ item.toString }}",
+           "errors" => false
+        ],
+         [
+            "document"=> Notification::class,
+            "requestType" => "update",
+            "detail" => "{{ item.toString }}",
+            "errors" => true
+         ],
+         [
+            "document"=> Notification::class,
+            "requestType" => "delete",
+            "detail" => "{{ item.toString }}",
+            "errors" => false
+         ],
       ];
-      $this->data['description']= [
-         'value'=> 'DESCRIPTION',
-         'status'=> 'STATUS',
-         'size'=> 'SIZE'
-      ];
    }
 
-   public function getTitle() {
-      return $this->data['title'];
+   public function getActions():array {
+      return $this->data['actions'];
    }
 
-   public function getDescription() {
-      return $this->data['description'];
+   public function getCreateIndicator():array {
+      return $this->getIndicator("create");
    }
-
+   public function getReadIndicator():array {
+      return $this->getIndicator("read");
+   }
+   public function getUpdateIndicator():array {
+      return $this->getIndicator("update");
+   }
+   public function getDeleteIndicator():array {
+      return $this->getIndicator("delete");
+   }
+   public function getIndicator($requestType):array {
+      $nbOk = collect($this->data['actions'])->filter(function($item) use ($requestType){
+         return !$item["errors"] && $item["requestType"]==$requestType;
+      })->count();
+      $nbKo = collect($this->data['actions'])->filter(function($item) use ($requestType){
+         return $item["errors"] && $item["requestType"]==$requestType;
+      })->count();
+      return ["ok"=>$nbOk, "ko"=>$nbKo];
+   }
    /**
     * @inheritDoc
     */
