b27f2042479049bc2782d1778e8e0d19e1a438e5
MarcoDec <marc.declercq@laposte.net>
2021-12-12 20:45:46 +0100

    Correction attribut Notification

--- src/Entity/Management/Notification.php
+++ src/Entity/Management/Notification.php
@@ -2,91 +2,100 @@
 
 namespace App\Entity\Management;
 
+use App\Attribute\Couchdb\Abstract\Fetch;
 use App\Attribute\Couchdb\Document;
+use App\Attribute\Couchdb\ORM\ManyToOne;
+use App\Entity\Hr\Employee\Employee;
 use JetBrains\PhpStorm\Pure;
 
 #[Document]
-class Notification
-{
-   public int $id;
-   public string $subject;
-   public string $category;
-   public bool $read;
+class Notification {
+    public string $category;
+    public int $id;
+    public bool $read;
+    public string $subject;
+
+    #[ManyToOne(Employee::class, Fetch::LAZY)]
+   public Employee $user;
+
+    public function __construct() {
+    }
+
+    #[Pure]
+    public function __toString(): string {
+        return $this->getId().' - '.$this->getCategory().': '.$this->getSubject();
+    }
 
    /**
     * @return string
     */
-   public function getSubject(): string
-   {
-      return $this->subject;
-   }
+    public function getCategory(): string {
+        return $this->category;
+    }
 
    /**
-    * @param string $subject
-    * @return Notification
+    * @return int
     */
-   public function setSubject(string $subject): self
-   {
-      $this->subject = $subject;
-      return $this;
-   }
+    public function getId(): int {
+        return $this->id;
+    }
 
    /**
     * @return string
     */
-   public function getCategory(): string
-   {
-      return $this->category;
-   }
+    public function getSubject(): string {
+        return $this->subject;
+    }
+
+   /**
+    * @return Employee
+    */
+    public function getUser(): Employee {
+        return $this->user;
+    }
+
+   /**
+    * @return bool
+    */
+    public function isRead(): bool {
+        return $this->read;
+    }
 
    /**
     * @param string $category
     * @return Notification
     */
-   public function setCategory(string $category): self
-   {
-      $this->category = $category;
-      return $this;
-   }
+    public function setCategory(string $category): self {
+        $this->category = $category;
+        return $this;
+    }
 
-   /**
-    * @return bool
-    */
-   public function isRead(): bool
-   {
-      return $this->read;
-   }
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
 
    /**
     * @param bool $read
     * @return Notification
     */
-   public function setRead(bool $read): self
-   {
-      $this->read = $read;
-      return $this;
-   }
+    public function setRead(bool $read): self {
+        $this->read = $read;
+        return $this;
+    }
 
    /**
-    * @return int
+    * @param string $subject
+    * @return Notification
     */
-   public function getId(): int
-   {
-      return $this->id;
-   }
+    public function setSubject(string $subject): self {
+        $this->subject = $subject;
+        return $this;
+    }
 
    /**
-    * @param int $id
+    * @param Employee $user
     */
-   public function setId(int $id): void
-   {
-      $this->id = $id;
-   }
-
-
-
-   #[Pure] public function __toString():string {
-      return $this->getId().' - '.$this->getCategory().': '.$this->getSubject();
-   }
-
+    public function setUser(Employee $user): void {
+        $this->user = $user;
+    }
 }
