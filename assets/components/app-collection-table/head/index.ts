3c125c0407dc90b8e6630c826ce44eab031a7734
Florent8 <florent.martin1997@orange.fr>
2021-12-21 15:03:10 +0100

    fix: Reprise des tries

--- assets/components/app-collection-table/head/index.ts
+++ assets/components/app-collection-table/head/index.ts
@@ -2,6 +2,7 @@ import type {Component} from 'vue'
 import {defineAsyncComponent} from 'vue'
 
 export const AppCollectionTableAdd = defineAsyncComponent<Component>(async () => import('./AppCollectionTableAdd.vue'))
+export const AppCollectionTableField = defineAsyncComponent<Component>(async () => import('./AppCollectionTableField.vue'))
 export const AppCollectionTableFields = defineAsyncComponent<Component>(async () => import('./AppCollectionTableFields.vue'))
 export const AppCollectionTableHeaders = defineAsyncComponent<Component>(async () => import('./AppCollectionTableHeaders.vue'))
 export const AppCollectionTableSearch = defineAsyncComponent<Component>(async () => import('./AppCollectionTableSearch.vue'))
