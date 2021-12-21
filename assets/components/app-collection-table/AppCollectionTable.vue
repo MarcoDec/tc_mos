3b813dafb57c8c4baaaa6e1744c5a4e0921e0880
Florent8 <florent.martin1997@orange.fr>
2021-12-21 16:16:59 +0100

    AppCollectionTable : Calcul du nombre de pages

--- assets/components/app-collection-table/AppCollectionTable.vue
+++ assets/components/app-collection-table/AppCollectionTable.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
     import type {TableField, TableItem} from '../../types/app-collection-table'
-    import {defineProps, provide} from 'vue'
+    import {computed, defineProps, provide} from 'vue'
     import type {PropType} from 'vue'
 
     const props = defineProps({
         create: {required: false, type: Boolean},
+        currentPage: {default: 1, type: Number},
         fields: {required: true, type: Object as PropType<TableField>},
         id: {required: true, type: String},
         items: {required: true, type: Array as PropType<TableItem[]>},
         pagination: {required: false, type: Boolean}
     })
+    const count = computed(() => props.items.length)
     provide('create', props.create)
     provide('fields', props.fields)
     provide('table-id', props.id)
@@ -20,5 +22,5 @@
         <AppCollectionTableHeaders/>
         <AppCollectionTableItems :items="items"/>
     </table>
-    <AppPagination v-if="pagination" :count="100" :current="2" class="float-end"/>
+    <AppPagination v-if="pagination" :count="count" :current="currentPage" class="float-end"/>
 </template>
