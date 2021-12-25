100b319e3d22d9098478d0052ab408230c25e0b1
MarcoDec <marc.declercq@laposte.net>
2021-12-26 00:41:31 +0100

    fix ressource /api/employee indisponible

--- src/Entity/Hr/Employee/Employee.php
+++ src/Entity/Hr/Employee/Employee.php
@@ -2,12 +2,22 @@
 
 namespace App\Entity\Hr\Employee;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Entity\Security\User;
 use App\Entity\Traits\NameTrait;
 use App\Repository\Hr\Employee\EmployeeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+#[
+   ORM\Entity(repositoryClass: EmployeeRepository::class),
+   ApiResource(
+      collectionOperations: [],
+      itemOperations: ['get' => ['openapi_context' => [
+         'description' => 'Récupère un utilisateur selon son id',
+         'summary' => 'Récupère un utilisateur',
+      ]]],
+      normalizationContext: ['groups' => ['read:user'], 'openapi_definition_name' => 'User-read'])
+]
 class Employee extends User {
     use NameTrait;
 }
