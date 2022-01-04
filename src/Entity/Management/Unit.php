d354c3ba132991ceeffedde6756dbd2b6cf04f28
Florent8 <florent.martin1997@orange.fr>
2022-01-04 13:43:53 +0100

    fix: Currency & Unit inheritance

--- src/Entity/Management/Unit.php
+++ src/Entity/Management/Unit.php
@@ -7,17 +7,24 @@ use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Entity\Embeddable\Hr\Employee\Roles;
-use App\Entity\Entity;
-use App\Entity\Traits\NameTrait;
 use App\Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use JetBrains\PhpStorm\Pure;
 use Symfony\Component\Serializer\Annotation as Serializer;
-use Symfony\Component\Validator\Constraints as Assert;
-use Tightenco\Collect\Support\Collection as LaravelCollection;
 
+/**
+ * @method self            addChild(self $children)
+ * @method Collection<int, self> getChildren()
+ * @method float           getConvertorDistance(self $unit)
+ * @method null|self       getParent()
+ * @method bool            has(null|self $unit)
+ * @method bool            isLessThan(self $unit)
+ * @method self            removeChild(self $children)
+ * @method self            setBase(float $base)
+ * @method self            setCode(null|string $code)
+ * @method self            setName(null|string $name)
+ * @method self            setParent(null|self $parent)
+ */
 #[
     ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial', 'code' => 'partial']),
     ApiResource(
@@ -67,158 +74,20 @@ use Tightenco\Collect\Support\Collection as LaravelCollection;
     UniqueEntity('code'),
     UniqueEntity('name')
 ]
-class Unit extends Entity {
-    use NameTrait;
-
-    #[
-        ApiProperty(description: 'Nom', required: true, example: 'Gramme'),
-        Assert\NotBlank,
-        ORM\Column,
-        Serializer\Groups(['read:name', 'write:name'])
-    ]
-    protected ?string $name = null;
-
-    #[
-        ApiProperty(description: 'Base', required: true, example: 1),
-        Assert\NotBlank,
-        Assert\Positive,
-        ORM\Column(options: ['default' => 1]),
-        Serializer\Groups(['read:unit', 'write:unit'])
-    ]
-    private float $base = 1;
-
+class Unit extends AbstractUnit {
     /** @var Collection<int, self> */
     #[
         ApiProperty(description: 'Enfants ', readableLink: false, example: ['/api/units/2', '/api/units/3']),
         ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class),
         Serializer\Groups(['read:unit'])
     ]
-    private Collection $children;
+    protected Collection $children;
 
-    #[
-        ApiProperty(description: 'Code ', required: true, example: 'g'),
-        Assert\NotBlank,
-        ORM\Column,
-        Serializer\Groups(['read:unit', 'write:unit'])
-    ]
-    private ?string $code = null;
-
+    /** @var null|self */
     #[
         ApiProperty(description: 'Parent ', readableLink: false, example: '/api/units/1'),
         ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children'),
         Serializer\Groups(['read:unit', 'write:unit'])
     ]
-    private ?self $parent = null;
-
-    #[Pure]
-    public function __construct() {
-        $this->children = new ArrayCollection();
-    }
-
-    final public function addChild(self $children): self {
-        if (!$this->children->contains($children)) {
-            $this->children->add($children);
-            $children->setParent($this);
-        }
-        return $this;
-    }
-
-    final public function getBase(): float {
-        return $this->base;
-    }
-
-    /**
-     * @return Collection<int, self>
-     */
-    final public function getChildren(): Collection {
-        return $this->children;
-    }
-
-    final public function getCode(): ?string {
-        return $this->code;
-    }
-
-    #[Pure]
-    final public function getConvertorDistance(self $unit): float {
-        $distance = $this->getDistance($unit);
-        return $this->isLessThan($unit) ? 1 / $distance : $distance;
-    }
-
-    final public function getParent(): ?self {
-        return $this->parent;
-    }
-
-    final public function has(?self $unit): bool {
-        return $unit !== null && $this->getFamily()->contains(static fn (self $member): bool => $member->getId() === $unit->getId());
-    }
-
-    #[Pure]
-    final public function isLessThan(self $unit): bool {
-        return $this->getLess($unit) === $this;
-    }
-
-    final public function removeChild(self $children): self {
-        if ($this->children->contains($children)) {
-            $this->children->removeElement($children);
-            if ($children->getParent() === $this) {
-                $children->setParent(null);
-            }
-        }
-        return $this;
-    }
-
-    final public function setBase(float $base): self {
-        $this->base = $base;
-        return $this;
-    }
-
-    final public function setCode(?string $code): self {
-        $this->code = $code;
-        return $this;
-    }
-
-    final public function setParent(?self $parent): self {
-        $this->parent = $parent;
-        return $this;
-    }
-
-    /**
-     * @return LaravelCollection<int, self>
-     */
-    private function getDepthChildren(): LaravelCollection {
-        /** @phpstan-ignore-next-line */
-        return collect($this->children->getValues())
-            ->map(static fn (self $child): array => $child->getDepthChildren()->push($child)->values()->all())
-            ->flatten()
-            ->unique->getId()
-            ->values();
-    }
-
-    #[Pure]
-    private function getDistance(self $unit): float {
-        return $this->getDistanceBase() * $unit->getDistanceBase();
-    }
-
-    private function getDistanceBase(): float {
-        return $this->base > 1 ? $this->base : 1 / $this->base;
-    }
-
-    /**
-     * @return LaravelCollection<int, self>
-     */
-    private function getFamily(): LaravelCollection {
-        return $this->getRoot()->getDepthChildren();
-    }
-
-    private function getLess(self $unit): self {
-        return $this->base < $unit->base ? $this : $unit;
-    }
-
-    private function getRoot(): self {
-        $root = $this;
-        while ($root->parent !== null) {
-            $root = $root->parent;
-        }
-        return $root;
-    }
+    protected $parent;
 }
