f88760438e24f7c89c5ca4ca26c3514b22af0e25
Matthieu <12222956+Sehkmat@users.noreply.github.com>
2022-01-04 19:14:43 +0100

    Warehouse + CompanyTrait

---
+++ src/Entity/Traits/CompanyTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use App\Entity\Management\Society\Company;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation as Serializer;
+
+trait CompanyTrait {
+    #[
+        ApiProperty(description: 'Company', required: false, example: '/api/companies/1'),
+        ORM\ManyToOne(fetch: 'EAGER', targetEntity: Company::class),
+        Serializer\Groups(['read:company', 'write:incotcompanyerms'])
+    ]
+    protected ?Company $company;
+
+    final public function getCompany(): ?Company {
+        return $this->company;
+    }
+
+    final public function setCompany(?Company $company): self {
+        $this->company = $company;
+        return $this;
+    }
+}
