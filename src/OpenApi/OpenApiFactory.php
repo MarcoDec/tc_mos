e855a5bf75a72422958b287dfdbf276467e04c85
Florent8 <florent.martin1997@orange.fr>
2022-03-24 15:00:47 +0100

    Revert "OpenApi : Schema d'une Resource"
    This reverts commit 9dd305101d66bf7229ce4502ad33ffc4c3452140.

--- src/OpenApi/OpenApiFactory.php
+++ src/OpenApi/OpenApiFactory.php
@@ -6,9 +6,6 @@ use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
 use ApiPlatform\Core\OpenApi\Model;
 use ApiPlatform\Core\OpenApi\OpenApi;
 use ApiPlatform\Core\OpenApi\Options;
-use App\ApiPlatform\Core\Annotation\ApiProperty;
-use ArrayObject;
-use JetBrains\PhpStorm\Pure;
 
 final class OpenApiFactory implements OpenApiFactoryInterface {
     public function __construct(private readonly Options $options) {
@@ -22,24 +19,10 @@ final class OpenApiFactory implements OpenApiFactoryInterface {
             info: $this->getInfo(),
             servers: [],
             paths: new Model\Paths(),
-            components: new Model\Components(schemas: $this->generateSchemas())
+            components: new Model\Components()
         );
     }
 
-    /**
-     * @return ArrayObject<string, Schema>
-     */
-    private function generateSchemas(): ArrayObject {
-        return new ArrayObject(['Resource' => new Schema(
-            description: 'Base d\'une resource',
-            properties: [
-                '@id' => new ApiProperty(readOnly: true, required: true),
-                '@type' => new ApiProperty(readOnly: true, required: true)
-            ]
-        )]);
-    }
-
-    #[Pure]
     private function getInfo(): Model\Info {
         return new Model\Info(
             title: $this->options->getTitle(),
