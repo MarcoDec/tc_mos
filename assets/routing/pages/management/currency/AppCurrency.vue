471b6b6936b8cd061707ab0b18fd4177000e0a6a
Florent8 <florent.martin1997@orange.fr>
2022-04-29 09:41:45 +0200

    Préparation pré-fusion avec feature/vue/gui

--- assets/routing/pages/management/currency/AppCurrency.vue
+++ assets/routing/pages/management/currency/AppCurrency.vue
@@ -1,102 +1,87 @@
-<script lang="ts" setup>
-import * as Cookies from '../../../../cookie'
-import {
-  useNamespacedActions,
-  useNamespacedGetters,
-  useNamespacedMutations,
-} from "vuex-composition-helpers";
-import {Actions, ActionTypes, Mutations, MutationTypes} from "../../../../store/management/currency";
-import {onMounted, ref} from "vue";
-import AppForm from "../../../../components/bootstrap-5/form/AppForm.vue";
-import AppCol from "../../../../components/bootstrap-5/layout/AppCol.vue";
-import {FormField} from "../../../../types/bootstrap-5";
+<script setup>
+    import * as Cookies from '../../../../cookie'
+    import {useNamespacedActions, useNamespacedGetters, useNamespacedMutations} from 'vuex-composition-helpers'
+    import {ActionTypes, MutationTypes} from '../../../../store/management/currency'
+    import {onMounted, ref} from 'vue'
 
-const fields: FormField[] = [
-  {label: 'Actif', name: 'active', type: 'switch'},
-  {label: 'Code', name: 'code'},
-  {label: 'Nom', name: 'nom'},
-]
+    const fields = [
+        {label: 'Actif', name: 'active', type: 'switch'},
+        {label: 'Code', name: 'code'},
+        {label: 'Nom', name: 'nom'}
+    ]
+    const selected = ref()
+    onMounted(async () => {
+        await useNamespacedActions('currency', [ActionTypes.FETCH_CURRENCY])[ActionTypes.FETCH_CURRENCY]()
+    })
+    const list = useNamespacedGetters('currency', ['getListCurrency']).getListCurrency
+    const filter = useNamespacedGetters('currency', ['filters']).filters
+    const actions = useNamespacedActions('currency', [ActionTypes.UPDATE_DATA])[ActionTypes.UPDATE_DATA]
+    const search = useNamespacedMutations('currency', [MutationTypes.FILTER])[MutationTypes.FILTER]
 
-const selected = ref()
-
-onMounted(async () => {
-  await useNamespacedActions<Actions>('currency', [ActionTypes.FETCH_CURRENCY])[ActionTypes.FETCH_CURRENCY]()
-
-});
-const list = useNamespacedGetters('currency', ['getListCurrency']).getListCurrency
-const filter = useNamespacedGetters('currency', ['filters']).filters
-const actions = useNamespacedActions<Actions>('currency', [ActionTypes.UPDATE_DATA])[ActionTypes.UPDATE_DATA]
-const search = useNamespacedMutations<Mutations>('currency', [MutationTypes.FILTER])[MutationTypes.FILTER]
-
-async function click(item: PointerEvent) {
-  if (!(item instanceof PointerEvent))
-
-    selected.value = item
-  Cookies.set('idItem', selected.value)
-  await actions({
-    active: !list.value[selected.value - 1].active
-  })
-}
-
+    async function click(item) {
+        if (!(item instanceof PointerEvent))
+            selected.value = item
+        Cookies.set('idItem', selected.value)
+        await actions({
+            active: !list.value[selected.value - 1].active
+        })
+    }
 </script>
 
 <template>
-  <AppRow>
     <AppRow>
-      <Fa icon="euro-sign" class="iconEuro"/>
-      <h1 class="textD">Devise</h1>
+        <AppRow>
+            <Fa class="iconEuro" icon="euro-sign"/>
+            <h1 class="textD">
+                Devise
+            </h1>
+        </AppRow>
+        <AppRow>
+            <AppCol>
+                <AppForm :fields="fields" :values="filter" class="formDevise" @update:values="search"/>
+            </AppCol>
+        </AppRow>
     </AppRow>
-    <AppRow>
-      <AppCol>
-        <AppForm :values="filter" @update:values="search" :fields="fields" class="formDevise"/>
-      </AppCol>
-
+    <AppRow class="divDevise">
+        <AppCol v-for="list in list">
+            <div class="form-check form-switch">
+                <input
+                    :checked="list.active" class="form-check-input" type="checkbox"
+                    @click="click(list.id)"/>
+                <span>{{ list.code }}</span><br/>
+                <span>1 € = {{ list.rate }} {{ list.symbol }}</span><br/>
+                <span>{{ list.name }}</span>
+            </div>
+        </AppCol>
     </AppRow>
-  </AppRow>
-  <AppRow class="divDevise">
-    <AppCol v-for="list in list">
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" :checked="list.active"
-               @click="click(list.id)"
-        >
-        <span>{{ list.code }}</span><br>
-        <span>1 € = {{ list.rate }} {{ list.symbol }}</span><br>
-        <span>{{ list.name }}</span>
-      </div>
-    </AppCol>
-  </AppRow>
-
 </template>
 
-
 <style scoped>
-.row > * {
-  width: auto;
-}
+    .row > * {
+        width: auto;
+    }
 
-.textD {
-  font-size: 3.07rem;
-}
+    .textD {
+        font-size: 3.07rem;
+    }
 
-.divDevise {
-  border: 1px solid #2a2a4b;
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: repeat(5, 1fr);
-  margin-right: -15px;
-  margin-left: -15px;
-}
+    .divDevise {
+        border: 1px solid #2a2a4b;
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: repeat(5, 1fr);
+        margin-right: -15px;
+        margin-left: -15px;
+    }
 
-.formDevise {
-  display: flex;
-  flex-direction: row;
-  border: 1px solid #2a2a4b;
-}
+    .formDevise {
+        display: flex;
+        flex-direction: row;
+        border: 1px solid #2a2a4b;
+    }
 
-.iconEuro {
-  font-size: 3.07rem;
-  margin-top: 7px;
-}
-
-
+    .iconEuro {
+        font-size: 3.07rem;
+        margin-top: 7px;
+    }
 </style>
---
+++ assets/store/management/currency/index.js
@@ -0,0 +1,53 @@
+import {fetchApi} from '../../../api'
+
+export const module = {
+    actions: {
+        async fetchCurrency({commit}) {
+            const response = await fetchApi('/api/currencies', {
+                method: 'get',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${Cookies.get('token')}`}
+            })
+            const list = response['hydra:member']
+            commit('getCurrency', list)
+        },
+        async updateData({commit}, payload) {
+            const id = Cookies.get('idItem')
+            if (typeof id === 'undefined')
+                return
+            const response = await fetch(`/api/currencies/${id}`, {
+                body: JSON.stringify({
+                    active: payload.active
+                }),
+                method: 'patch',
+                headers: {
+                    'Content-Type': ' application/merge-patch+json',
+                    'Authorization': `Bearer ${Cookies.get('token')}`
+                }
+            })
+        }
+    },
+    getters: {
+        filters: state => ({code: state.code, nom: state.nom, active: state.active}),
+        getListCurrency: state => state.list
+            .filter(currency => currency.code.toUpperCase().startsWith(state.code.toUpperCase()))
+            .filter(currency => (typeof currency.name === 'string' ? currency.name.toUpperCase().startsWith(state.nom.toUpperCase()) : null))
+            .filter(currency => currency.active === state.active)
+    },
+    mutations: {
+        filter(state, {code, nom, active}) {
+            state.nom = nom,
+            state.code = code,
+            state.active = active
+        },
+        getCurrency(state, liste) {
+            state.list = liste
+        }
+    },
+    namespaced: true,
+    state: {
+        active: false,
+        code: '',
+        list: [],
+        nom: ''
+    }
+}
