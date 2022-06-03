c01004bf78b091e9714cf75fa4d894e026fe1d3e
Florent8 <florent.martin1997@orange.fr>
2022-06-03 11:50:43 +0200

    rm assets-old

--- assets-old/components/bootstrap-5/index.js
+++
@@ -1,7 +0,0 @@
-export * from './form'
-export * from './layout'
-export * from './pagination'
-export {default as AppAlert} from './AppAlert'
-export {default as AppBadge} from './AppBadge'
-export {default as AppBtn} from './AppBtn'
-export {default as AppCard} from './AppCard'
