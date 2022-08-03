04c33bed020334381cc11db26f7423a7faa118c6
Florent8 <florent.martin1997@orange.fr>
2022-08-03 23:36:40 +0200

    Rename migration for merge

--- migrations/Version20220803210619.php
+++ migrations/Version20220803210619.php
@@ -19,7 +19,7 @@ use Symfony\Component\Intl\Currencies;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\UnicodeString;
 
-final class Version20220803173652 extends AbstractMigration {
+final class Version20220803210619 extends AbstractMigration {
     private UserPasswordHasherInterface $hasher;
 
     /** @var Collection<int, string> */
