fe88f38cc220dd52f6cad82b6fed6c9d92fb6ec8
firasmlika <firasmlika95@gmail.com>
2022-08-18 09:37:05 +0100

    la branche overview  terminée

--- assets/router/pages/AppOverview.vue
+++ assets/router/pages/AppOverview.vue
@@ -7,12 +7,12 @@
     <div class="bg-secondary card">
         <div class="row">
             <div class="col">
+                count: {{ storeOverview.count }}
+            </div>
+            <div class="col">
                 <div v-for="(item, index) in storeOverview.items" :key="index" class="col">
                     <span> {{ item.name }} : {{ item.value }}  </span>
                 </div>
-            </div>
-            <div class="col">
-                count: {{ storeOverview.count }}
             </div>
         </div>
     </div>
