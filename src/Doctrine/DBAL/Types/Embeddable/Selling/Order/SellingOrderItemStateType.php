6e096d43a8e3b31a084540c6b64282dbdb6eacc9
Florent8 <florent.martin1997@orange.fr>
2022-09-09 11:20:55 +0200

    SellingOrderItem fix: workflow

--- src/Doctrine/DBAL/Types/Embeddable/Selling/Order/SellingOrderItemStateType.php
+++ src/Doctrine/DBAL/Types/Embeddable/Selling/Order/SellingOrderItemStateType.php
@@ -7,8 +7,10 @@ use App\Doctrine\DBAL\Types\Embeddable\StateType;
 final class SellingOrderItemStateType extends StateType {
     final public const TYPES = [
         self::TYPE_STATE_AGREED,
+        self::TYPE_STATE_BILLED,
         self::TYPE_STATE_DELIVERED,
         self::TYPE_STATE_DRAFT,
+        self::TYPE_STATE_PAID,
         self::TYPE_STATE_PARTIALLY_DELIVERED
     ];
 
