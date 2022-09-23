17ce7e414e7d56b6f172866231e9568032162976
Florent8 <florent.martin1997@orange.fr>
2022-09-23 16:54:28 +0200

    /api/suppliers/receipts : Épuration des commandes sans ligne à réceptionner

--- src/Repository/Purchase/Supplier/SupplierRepository.php
+++ src/Repository/Purchase/Supplier/SupplierRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository\Purchase\Supplier;
 
 use App\Doctrine\DBAL\Types\Embeddable\BlockerStateType;
 use App\Doctrine\DBAL\Types\Embeddable\CloserStateType;
+use App\Doctrine\DBAL\Types\Embeddable\Purchase\Order\CloserStateType as ItemCloserStateType;
+use App\Doctrine\DBAL\Types\Embeddable\Purchase\Order\ItemStateType;
 use App\Doctrine\DBAL\Types\Embeddable\Purchase\Order\OrderStateType;
 use App\Entity\Purchase\Supplier\Supplier;
 use App\Security\SecurityTrait;
@@ -49,6 +51,17 @@ final class SupplierRepository extends ServiceEntityRepository {
                 )
             )
             ->setParameter('company', $this->getCompanyId())
+            ->innerJoin(
+                join: 'o.items',
+                alias: 'i',
+                conditionType: Join::WITH,
+                condition: sprintf(
+                    'i.embBlocker.state != \'%s\' AND i.embState.state IN (\'%s\', \'%s\')',
+                    ItemCloserStateType::TYPE_STATE_CLOSED,
+                    ItemStateType::TYPE_STATE_AGREED,
+                    ItemStateType::TYPE_STATE_PARTIALLY_DELIVERED
+                )
+            )
             ->where(sprintf('s.embBlocker.state != \'%s\'', BlockerStateType::TYPE_STATE_DISABLED))
             ->getQuery()
             ->getResult();
