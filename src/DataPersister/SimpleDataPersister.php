b73e5b5826cd3afe2872022cfe62bf1a8eb70aad
Florent8 <florent.martin1997@orange.fr>
2022-10-11 09:40:33 +0200

    Optimize CRUD VatMessage

--- src/DataPersister/SimpleDataPersister.php
+++ src/DataPersister/SimpleDataPersister.php
@@ -4,29 +4,31 @@ namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\Entity\Logistics\Stock\Stock;
-use App\Entity\Project\Product\Product;
-use App\Entity\Purchase\Component\Component;
+use App\Entity\Management\VatMessage;
 use Doctrine\ORM\EntityManagerInterface;
 
+/**
+ * @phpstan-type Data Stock<\App\Entity\Purchase\Component\Component|\App\Entity\Project\Product\Product>|VatMessage
+ */
 final class SimpleDataPersister implements ContextAwareDataPersisterInterface {
     public function __construct(private readonly EntityManagerInterface $em) {
     }
 
     /**
-     * @param Stock<Component|Product> $data
-     * @param mixed[]                  $context
+     * @param Data    $data
+     * @param mixed[] $context
      *
-     * @return Stock<Component|Product>
+     * @return Data
      */
-    public function persist($data, array $context = []): Stock {
+    public function persist($data, array $context = []): Stock|VatMessage {
         $this->em->persist($data);
         $this->em->flush();
         return $data;
     }
 
     /**
-     * @param Stock<Component|Product> $data
-     * @param mixed[]                  $context
+     * @param Data    $data
+     * @param mixed[] $context
      */
     public function remove($data, array $context = []): void {
     }
@@ -35,7 +37,7 @@ final class SimpleDataPersister implements ContextAwareDataPersisterInterface {
      * @param mixed[] $context
      */
     public function supports($data, array $context = []): bool {
-        return $data instanceof Stock
+        return ($data instanceof Stock || $data instanceof VatMessage)
             && (
                 (isset($context['collection_operation_name']) && in_array($context['collection_operation_name'], ['post', 'receipt']))
                 || (isset($context['item_operation_name']) && in_array($context['item_operation_name'], ['out', 'patch']))
