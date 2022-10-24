48a2f44df8abb8f9f7c16fd6a5a6cc053ede5c3b
Florent8 <florent.martin1997@orange.fr>
2022-10-24 14:36:42 +0200

    IHM AppShowGui : Récupération brute

---
+++ assets/components/tabs/AppTabs.vue
@@ -0,0 +1,65 @@
+<script setup>
+    import {computed, onMounted, onUnmounted, provide, ref, shallowRef, watch} from 'vue'
+    import AppTabBtn from './AppTabBtn.vue'
+    import {Tab as BTab} from 'bootstrap'
+
+    const props = defineProps({
+        icon: {type: Boolean},
+        iconSwitch: {type: Boolean},
+        id: {required: true, type: String},
+        vertical: {type: Boolean}
+    })
+    const bTab = shallowRef(null)
+    const divFlex = computed(() => `flex-${props.vertical ? 'row' : 'column'}`)
+    const el = ref()
+    const iconMode = ref(false)
+    const iconSwitchId = computed(() => `${props.id}-icon-switch`)
+    const tabs = ref([])
+    const tabsSize = computed(() => `${props.vertical ? 'w' : 'h'}-90`)
+    const ulCss = computed(() => `flex-${props.vertical ? 'column' : 'row'} ${props.vertical ? 'w' : 'h'}-10`)
+
+    function dispose() {
+        if (bTab.value !== null) {
+            bTab.value.dispose()
+            bTab.value = null
+        }
+    }
+
+    function instantiate() {
+        if (typeof el.value === 'undefined')
+            return
+        dispose()
+        bTab.value = new BTab(el.value)
+    }
+
+    provide('tabs', tabs)
+
+    onMounted(instantiate)
+    onUnmounted(dispose)
+
+    watch(tabs, instantiate)
+
+    watch(() => props.icon, icon => {
+        iconMode.value = icon
+    })
+</script>
+
+<template>
+    <div :id="id" :class="divFlex" class="d-flex">
+        <ul ref="el" :class="ulCss" class="bg-white d-flex nav nav-tabs" role="tablist">
+            <li v-show="iconSwitch" class="form-check form-switch nav-item" role="presentation">
+                <input :id="iconSwitchId" v-model="iconMode" class="form-check-input" type="checkbox"/>
+                <label :for="iconSwitchId" class="form-check-label">
+                    <Fa icon="icons"/>
+                    <template v-if="!iconMode">
+                        Icône
+                    </template>
+                </label>
+            </li>
+            <AppTabBtn v-for="tab in tabs" :key="tab.labelledby" :icon="iconMode" :tab="tab"/>
+        </ul>
+        <div :class="tabsSize" class="bg-white overflow-hidden tab-content">
+            <slot/>
+        </div>
+    </div>
+</template>
---
+++ assets/stores/gui.js
@@ -0,0 +1,110 @@
+import AppShowGuiResizableCard from '../components/gui/AppShowGuiResizableCard.vue'
+import {defineStore} from 'pinia'
+
+export default defineStore('gui', {
+    actions: {
+        disableDrag() {
+            this.dragged = false
+        },
+        drag() {
+            const drag = ({y}) => {
+                this.setRatio((this.innerHeight - y + this.marginTop) / this.innerHeight)
+            }
+
+            const stopDrag = () => {
+                this.disableDrag()
+                document.documentElement.removeEventListener('mousemove', drag)
+                document.documentElement.removeEventListener('mouseup', stopDrag)
+            }
+
+            document.documentElement.addEventListener('mousemove', drag)
+            document.documentElement.addEventListener('mouseup', stopDrag)
+        },
+        enableDrag() {
+            this.dragged = true
+        },
+        resize(el) {
+            const rect = el.getBoundingClientRect()
+            this.top = rect.top
+            if (window.top !== null) {
+                this.height = window.top.innerHeight - rect.top - 5
+                this.width = window.top.innerWidth - 25
+                this.windowWidth = window.top.innerWidth
+                if (this.windowWidth < 1140)
+                    this.setRatio(0.5)
+            }
+        },
+        setRatio(ratio) {
+            if (ratio >= 0.1 && ratio <= 0.9)
+                this.ratio = ratio
+        }
+    },
+    getters: {
+        bottomHeight(state) {
+            return Math.round(this.marginedInnerHeight * state.ratio)
+        },
+        bottomHeightPx() {
+            return `${this.bottomHeight}px`
+        },
+        endWidth() {
+            return Math.round(this.marginedInnerWidth - this.startWidth)
+        },
+        endWidthPx() {
+            return `${this.endWidth}px`
+        },
+        guiBottom: state => (state.windowWidth >= 1140 ? AppShowGuiResizableCard : 'AppShowGuiCard'),
+        heightPx: state => `${state.height}px`,
+        icon: state => state.windowWidth <= 800,
+        innerBottomHeight(state) {
+            return this.bottomHeight - 2 * state.padding
+        },
+        innerBottomHeightPx() {
+            return `${this.innerBottomHeight}px`
+        },
+        innerHeight: state => Math.round(state.height - 2 * state.padding),
+        innerStartHeight(state) {
+            return this.topHeight - 2 * state.padding
+        },
+        innerStartHeightPx() {
+            return `${this.innerStartHeight}px`
+        },
+        innerWidth: state => Math.round(state.width - 2 * state.padding),
+        innerWidthPx() {
+            return `${this.innerWidth}px`
+        },
+        marginEndPx: state => `${state.marginEnd}px`,
+        marginTopPx: state => `${state.marginTop}px`,
+        marginedInnerHeight(state) {
+            return this.innerHeight - state.marginTop
+        },
+        marginedInnerWidth(state) {
+            return this.innerWidth - state.marginEnd
+        },
+        paddingPx: state => `${state.padding}px`,
+        startWidth() {
+            return Math.round(this.marginedInnerWidth / 2)
+        },
+        startWidthPx() {
+            return `${this.startWidth}px`
+        },
+        topHeight() {
+            return Math.round(this.marginedInnerHeight - this.bottomHeight)
+        },
+        topHeightPx() {
+            return `${this.topHeight}px`
+        },
+        vertical: state => state.windowWidth <= 950,
+        widthPx: state => `${state.width}px`
+    },
+    state: () => ({
+        dragged: false,
+        height: 0,
+        marginEnd: 10,
+        marginTop: 10,
+        padding: 10,
+        ratio: 0.5,
+        top: 0,
+        width: 0,
+        windowWidth: 0
+    })
+})
