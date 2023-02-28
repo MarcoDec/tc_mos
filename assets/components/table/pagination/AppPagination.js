142b243c92c5fc15ed1004132a0b30ad8729359e
MarcoDec <marc.declercq@laposte.net>
2023-02-28 01:48:10 +0100

    fix code asset

---
+++ assets/components/table/pagination/AppPagination.js
@@ -0,0 +1,30 @@
+import {h, resolveComponent} from 'vue'
+
+function AppPagination(props, context) {
+    const range = Array.from(Array(props.store.pages + 1).keys())
+    range.shift()
+    return h(
+        'nav',
+        h(
+            'ul',
+            {class: 'pagination'},
+            typeof context.slots['default'] === 'function' ? context.slots['default']({machine: props.machine, range, store: props.store})
+                : range.map(index => h(
+                    resolveComponent('AppPaginationItem'),
+                    {
+                        index,
+                        async onClick() {
+                            props.machine.send('submit')
+                            await props.store.goTo(index)
+                            props.machine.send('success')
+                        },
+                        store: props.store
+                    }
+                ))
+        )
+    )
+}
+
+AppPagination.props = {machine: {required: true, type: Object}, store: {required: true, type: Object}}
+
+export default AppPagination
