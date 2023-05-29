fecb8f261636707bb4eb17f58e3ef5a9b2a7d1a9
marcodec <marc.declercq@laposte.net>
2023-05-29 10:38:05 +0200

    fix rendu de l'image en front desktop

--- assets/components/tree/card/AppTreeImg.vue
+++ assets/components/tree/card/AppTreeImg.vue
@@ -1,10 +1,23 @@
 <script setup>
-    defineProps({tree: {required: true, type: Object}})
+    import {computed} from 'vue'
+
+    const props = defineProps({tree: {required: true, type: Object}})
+    const selectedKey = computed(() => props.tree.selectedKey)
+    const hasImage = computed(() => {
+        if (typeof selectedKey.value === 'undefined' || isNaN(selectedKey.value)) return false
+        const nodeKey = selectedKey.value - 1
+        return props.tree.nodes[nodeKey].filePath
+    })
+    const imageUrl = computed(() => {
+        const nodeKey = selectedKey.value - 1
+        if (hasImage.value) return props.tree.nodes[nodeKey].filePath
+        return null
+    })
 </script>
 
 <template>
-    <img v-if="tree.hasIcon" :src="tree.icon"/>
+    <img v-if="hasImage" :src="imageUrl"/>
     <div v-else class="alert alert-primary">
-        Aucune icône
+        Aucune Image
     </div>
 </template>
