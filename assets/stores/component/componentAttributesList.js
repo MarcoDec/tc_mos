d90afaedbd0f41aefdba9038badd46b07aacd871
marcodec <marc.declercq@laposte.net>
2023-06-29 17:13:25 +0200

    Component::show Finalisation affichage et mise à jour des valeurs d'attributs

--- assets/stores/component/componentAttributesList.js
+++ assets/stores/component/componentAttributesList.js
@@ -11,15 +11,36 @@ export const useComponentAttributesStore = defineStore('componentAttributes', {
                 const item = generateComponentAttribute(attribute, this)
                 this.componentAttributes.push(item)
             }
+        },
+        async updateComponentAttributeColor(iri, newValue) {
+            this.item = {}
+            this.item = await api(iri, 'PATCH', {
+                color: newValue
+            })
+        },
+        async updateComponentAttributeMeasure(iri, newValue) {
+            this.item = {}
+            this.item = await api(iri, 'PATCH', {
+                measure: {
+                    code: newValue.code,
+                    denominator: newValue.denominator,
+                    value: Number(newValue.value)
+                }
+            })
+        },
+        async updateComponentAttributeValue(iri, newValue) {
+            this.item = {}
+            this.item = await api(iri, 'PATCH', {
+                value: newValue
+            })
         }
-
     },
     getters: {
 
     },
     state: () => ({
         componentAttributes: [],
+        item: {},
         items: []
-
     })
 })
