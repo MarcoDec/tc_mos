57040ebe1a52a5479e9f0a2f704baadddcb44f8d
marcodec <marc.declercq@laposte.net>
2023-07-05 13:02:31 +0200

    Employee::show Complétion de l'ensemble des roles possibles

--- assets/components/pages/hr/employee/AppShowEmployeeTabDroits.vue
+++ assets/components/pages/hr/employee/AppShowEmployeeTabDroits.vue
@@ -5,10 +5,42 @@
     const emit = defineEmits(['update', 'update:modelValue'])
     const fetchEmployeeStore = useEmployeeStore()
     await fetchEmployeeStore.fetchTeams()
-    const optionsRoles = [
-        {text: 'ROLE_USER', value: 'ROLE_USER'},
-        {text: 'ROLE_PRODUCTION_WRITER', value: 'ROLE_PRODUCTION_WRITER'}
+    const baseRoles = [
+        'ROLE_ACCOUNTING_READER',
+        'ROLE_ACCOUNTING_WRITER',
+        'ROLE_ACCOUNTING_ADMIN',
+        'ROLE_HR_READER',
+        'ROLE_HR_WRITER',
+        'ROLE_HR_ADMIN',
+        'ROLE_IT_ADMIN',
+        'ROLE_LEVEL_ANIMATOR',
+        'ROLE_LEVEL_DIRECTOR',
+        'ROLE_LEVEL_MANAGER',
+        'ROLE_LEVEL_OPERATOR',
+        'ROLE_LOGISTICS_READER',
+        'ROLE_LOGISTICS_WRITER',
+        'ROLE_LOGISTICS_ADMIN',
+        'ROLE_MAINTENANCE_READER',
+        'ROLE_MAINTENANCE_WRITER',
+        'ROLE_MAINTENANCE_ADMIN',
+        'ROLE_MANAGEMENT_READER',
+        'ROLE_MMANAGEMENT_WRITER',
+        'ROLE_MANAGEMENT_ADMIN',
+        'ROLE_PRODUCTION_READER',
+        'ROLE_PRODUCTION_WRITER',
+        'ROLE_PRODUCTION_ADMIN',
+        'ROLE_PROJECT_READER',
+        'ROLE_PROJECT_WRITER',
+        'ROLE_PROJECT_ADMIN',
+        'ROLE_PURCHASE_READER',
+        'ROLE_PURCHASE_WRITER', 'ROLE_PURCHASE_ADMIN',
+        'ROLE_QUALITY_READER', 'ROLE_QUALITY_WRITER', 'ROLE_QUALITY_ADMIN',
+        'ROLE_SELLING_READER', 'ROLE_SELLING_WRITER', 'ROLE_SELLING_ADMIN'
     ]
+    const optionsRoles = baseRoles.map(item => ({
+        text: item,
+        value: item
+    }))
     const droitFields = [
         {
             label: 'Role',
