c31f9687c6c9ed0c797269133851af28e1bd0de7
marcodec <marc.declercq@laposte.net>
2023-07-06 12:55:30 +0200

    Customer::show Création AppShowCustomerTabAccounting.vue

--- assets/router/pages/customer/AppCustomerFormShow.vue
+++ assets/router/pages/customer/AppCustomerFormShow.vue
@@ -1,5 +1,7 @@
 <script setup>
     import {computed, ref} from 'vue'
+    import AppShowCustomerTabAccounting
+        from '../../../components/pages/selling/customer/AppShowCustomerTabAccounting.vue'
     import AppShowCustomerTabGeneral from '../../../components/pages/selling/customer/AppShowCustomerTabGeneral.vue'
     import AppShowCustomerTabLogistic from '../../../components/pages/selling/customer/AppShowCustomerTabLogistic.vue'
     import AppShowCustomerTabQuality from '../../../components/pages/selling/customer/AppShowCustomerTabQuality.vue'
@@ -12,7 +14,6 @@
     import useOptions from '../../../stores/option/options'
     import {useRoute} from 'vue-router'
     import {useSocietyStore} from '../../../stores/societies/societies'
-    import {useSuppliersStore} from '../../../stores/supplier/suppliers'
 
     const route = useRoute()
     const idCustomer = route.params.id_customer
@@ -23,13 +24,11 @@
     await fecthOptions.fetchOp()
     const fetchCustomerStore = useCustomerStore()
     const fetchSocietyStore = useSocietyStore()
-    const fecthSuppliersStore = useSuppliersStore()
     const fetchCustomerAttachmentStore = useCustomerAttachmentStore()
     const fecthCustomerContactsStore = useCustomerContactsStore()
     await fetchCustomerStore.fetchOne(idCustomer)
     await fetchCustomerStore.fetchInvoiceTime()
     await fetchSocietyStore.fetch()
-    await fecthSuppliersStore.fetchVatMessage()
     const societyId = Number(fetchCustomerStore.customer.society.match(/\d+/))
     const customerId = Number(fetchCustomerStore.customer.id)
     await fetchSocietyStore.fetchById(societyId)
@@ -43,38 +42,11 @@
         ))
     // const dataSuppliers = computed(() =>
     //     Object.assign(fetchSuppliersStore.suppliers, fetchSocietyStore.society))
-    const dataCustomers = computed(() => ({
-        ...fetchCustomerStore.customer,
-        ...fetchSocietyStore.society,
-        ...fetchSocietyStore.incotermsValue,
-        ...fetchSocietyStore.vatMessageValue
-    }))
-    const optionsVatMessageForce = [
-        {
-            text: 'TVA par défaut selon le pays du client',
-            value: 'TVA par défaut selon le pays du client'
-        },
-        {text: 'Force AVEC TVA', value: 'Force AVEC TVA'},
-        {text: 'Force SANS TVA', value: 'Force SANS TVA'}
-    ]
+
     const optionsCountries = computed(() =>
         fecthOptions.options.map(op => {
             const text = op.text
             const value = op.id
-            const optionList = {text, value}
-            return optionList
-        }))
-    const optionsVat = computed(() =>
-        fecthSuppliersStore.vatMessage.map(op => {
-            const text = op.name
-            const value = op['@id']
-            const optionList = {text, value}
-            return optionList
-        }))
-    const optionsInvoice = computed(() =>
-        fetchCustomerStore.invoicesData.map(invoice => {
-            const text = invoice.name
-            const value = invoice['@id']
             const optionList = {text, value}
             return optionList
         }))
@@ -135,52 +107,6 @@
             type: 'text'
         }
     ]
-    const Comptabilitéfields = [
-        {label: 'compte de comptabilité', name: 'accountingAccount', type: 'text'},
-        {
-            label: 'forcer la TVA',
-            name: 'forceVat',
-            options: {
-                label: value =>
-                    optionsVatMessageForce.find(option => option.type === value)?.text
-                    ?? null,
-                options: optionsVatMessageForce
-            },
-            type: 'select'
-        },
-        {
-            label: 'montant de la facture minimum',
-            measure: {code: 'Devise', value: 'valeur'},
-            name: 'invoiceMin',
-            type: 'measure'
-        },
-        {
-            label: 'message TVA',
-            name: 'vatMessageValue',
-            options: {
-                label: value =>
-                    optionsVat.value.find(option => option.type === value)?.text ?? null,
-                options: optionsVat.value
-            },
-            type: 'select'
-        },
-        {
-            label: 'conditions de paiement',
-            name: 'paymentTerms',
-            options: {
-                label: value =>
-                    optionsInvoice.value.find(option => option.type === value)?.text
-                    ?? null,
-                options: optionsInvoice.value
-            },
-            type: 'select'
-        },
-        {label: 'Nombre de factures mensuel', name: 'nbInvoices', type: 'number'},
-        {label: 'url *', name: 'getUrl', type: 'text'},
-        {label: 'ident', name: 'getUsername', type: 'text'},
-        {label: 'password', name: 'getPassword', type: 'text'},
-        {label: 'Envoi facture par email', name: 'invoiceByEmail', type: 'boolean'}
-    ]
     const Adressefields = [
         {label: 'Email', name: 'getEmail', type: 'text'},
         {label: 'Adresse', name: 'getAddress', type: 'text'},
@@ -200,39 +126,6 @@
         },
         {label: 'Fax', name: 'getPhone', type: 'text'}
     ]
-
-    async function updateComp(value) {
-        const form = document.getElementById('addComptabilite')
-        const formData = new FormData(form)
-        const dataSociety = {
-            accountingAccount: formData.get('accountingAccount'),
-            forceVat: formData.get('forceVat'),
-            invoiceMin: {
-                code: 'EUR',
-                value: JSON.parse(formData.get('invoiceMin-value'))
-            },
-            vatMessage: formData.get('vatMessageValue')
-        }
-        const dataCustomer = {
-            accountingPortal: {
-                password: formData.get('getPassword'),
-                url: formData.get('getUrl'),
-                username: formData.get('getUsername')
-            },
-            invoiceByEmail: JSON.parse(formData.get('invoiceByEmail')),
-            nbInvoices: JSON.parse(formData.get('nbInvoices')),
-            paymentTerms: formData.get('paymentTerms')
-        }
-
-        const item = generateCustomer(value)
-        await item.updateAccounting(dataCustomer)
-
-        await fetchSocietyStore.update(dataSociety, societyId)
-        //   const itemSoc = generateSocieties(value);
-        //   await itemSoc.update(dataSociety);
-        await fetchSocietyStore.fetchById(societyId)
-        await fetchCustomerStore.fetchOne(idCustomer)
-    }
     async function updateAdresse(value) {
         const form = document.getElementById('addAdresses')
         const formData = new FormData(form)
@@ -380,11 +273,7 @@
             title="Comptabilité"
             icon="industry"
             tabs="gui-start">
-            <AppCardShow
-                id="addComptabilite"
-                :fields="Comptabilitéfields"
-                :component-attribute="dataCustomers"
-                @update="updateComp(dataCustomers)"/>
+            <Suspense><AppShowCustomerTabAccounting/></Suspense>
         </AppTab>
         <AppTab
             id="gui-start-addresses"
