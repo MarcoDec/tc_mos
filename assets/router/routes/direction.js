0b1544bd71c6901f789bf4bccaaba7222a176edf
mathieuCarvalho <voyage1aq2&@gmail.com>
2023-07-20 16:09:35 +0200

    changement de route

--- assets/router/routes/direction.js
+++ assets/router/routes/direction.js
@@ -1,6 +1,6 @@
 export default [
     {
-        component: async () => import('../pages/direction/AppSocietyShowInList.vue'),
+        component: async () => import('../../components/pages/Management/Society/bottom/AppSocietyShowInList.vue'),
         meta: {requiresAuth: true},
         name: 'directionsShow',
         path: '/society/show/:id_society'
