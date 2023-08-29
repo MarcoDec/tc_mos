829f78209c6ab65e2b98f4ebaad798c4e63c486d
marcodec <marc.declercq@laposte.net>
2023-08-29 15:07:51 +0200

    Warehouse:show Fin d'intégration modification champs de l'entité

--- assets/components/pages/logistic/warehouse/AppWarehouseFormShow.vue
+++ assets/components/pages/logistic/warehouse/AppWarehouseFormShow.vue
@@ -12,26 +12,37 @@
 
     const maRoute = useRoute()
     const warehouseId = maRoute.params.id_warehouse
+    const store = warehouseStore()
+    store.setCurrentId(warehouseId)
+    const probl = await store.fetch()
+    if (probl) maRoute.push({name: 'warehouse-list'}) //{props.item.id}\`
+
+    //region chargement des pièces jointes
     const warehouseAttachmentStore = useWarehouseAttachmentStore()
     warehouseAttachmentStore.fetchByElement(warehouseId)
+    //endregion
+    //region chargement des compagnies
     const fecthCompanyOptions = useOptions('companies')
     if (fecthCompanyOptions.hasOptions === false){
         await fecthCompanyOptions.fetchOp()
     }
-
-
-    const store = warehouseStore()
-    const storeCompanyAll = useSocietyListStore()
-    storeCompanyAll.fetch()
-
-    const optionsCompany = computed(() =>
-        fecthCompanyOptions.options.map(op => {
+    const optionsCompany = computed(() => fecthCompanyOptions.options.map(op => ({text: op.text, value: op['@id']})))
+    //endregion
+    //region chargement des options de destination
+    const fecthDestinationOptions = fecthCompanyOptions
+    const opEmpty = {
+        text: ' ',
+        '@id': null
+    }
+    fecthDestinationOptions.options.push(opEmpty)
+    const optionsDestination = computed(() =>
+        fecthDestinationOptions.options.map(op => {
             const text = op.text
             const value = op['@id']
             const optionList = {text, value}
             return optionList
         }))
-
+    //endregion
     function hasCamion(){
         const generalFamlies = store.getFamilies
         for (const el of generalFamlies){
@@ -41,105 +52,101 @@
         }
         return false
     }
-
-    let Generalitesfields = []
-    store.setCurrentId(warehouseId)
-    const probl = await store.fetch()
-    if (probl){
-        maRoute.push('/warehouse-list') //{props.item.id}\`
-    } else {
-        // store.setCurrentId(props.)
-        Generalitesfields = [
-            {label: 'Name', name: 'name', type: 'text'},
-            {
-                label: 'Company',
-                name: 'company',
-                options: {
-                    label: value =>
-                        optionsCompany.value.find(option => option.type === value)?.text
-                        ?? null,
-                    options: optionsCompany.value
-                },
-                type: 'select'
-            }
-        ]
-
-        if (hasCamion() === true){
-            const fecthDestinationOptions = fecthCompanyOptions
-            const opEmpty = {
-                text: ' ',
-                '@id': null
-            }
-            fecthDestinationOptions.options.push(opEmpty)
-            const optionsDestination = computed(() =>
-                fecthDestinationOptions.options.map(op => {
-                    const text = op.text
-                    const value = op['@id']
-                    const optionList = {text, value}
-                    return optionList
-                }))
-
-            Generalitesfields.push(
-                {
-                    label: 'Destination',
-                    name: 'destination',
-                    options: {
-                        label: value =>
-                            optionsDestination.value.find(option => option.type === value)?.text
-                            ?? null,
-                        options: optionsDestination.value
+    const Generalitesfields = [
+        {label: 'Name', name: 'name', type: 'text'},
+        {
+            label: 'Company',
+            name: 'company',
+            options: {
+                label: value =>
+                    optionsCompany.value.find(option => option.type === value)?.text
+                    ?? null,
+                options: optionsCompany.value
+            },
+            type: 'select'
+        },
+        {
+            label: 'Famille ',
+            name: 'families',
+            options: {
+                options: [
+                    {
+                        text: 'Prison',
+                        value: 'prison'
                     },
-                    type: 'select'
-                }
-            )
+                    {
+                        text: 'Production',
+                        value: 'production'
+                    },
+                    {
+                        text: 'Réception',
+                        value: 'réception'
+                    },
+                    {
+                        text: 'Magasin pièces finies',
+                        value: 'magasin pièces finies'
+                    },
+                    {
+                        text: 'Expédition',
+                        value: 'expédition'
+                    },
+                    {
+                        text: 'Magasin matières premières',
+                        value: 'magasin matières premières'
+                    },
+                    {
+                        text: 'Camion',
+                        value: 'camion'
+                    }
+                ]
+            },
+            type: 'multiselect'
         }
+    ]
+    if (hasCamion() === true) Generalitesfields.push(
+        {
+            label: 'Destination',
+            name: 'destination',
+            options: {
+                label: value =>
+                    optionsDestination.value.find(option => option.type === value)?.text
+                    ?? null,
+                options: optionsDestination.value
+            },
+            type: 'select'
+        }
+    )
 
-        Generalitesfields.push(
-            {
-                label: 'Famille ',
-                name: 'families',
-                options: {
-                    options: [
-                        {
-                            text: 'Prison',
-                            value: 'prison'
+    function localDataChange(value) {
+        console.log('localDataChange', value)
+        if (value.families.includes('camion')) {
+            //on doit s'assurer que les fields ont bien destination, sinon on ajoute
+            if (Generalitesfields.find(item => item.name !== 'destination')) {
+                Generalitesfields.push(
+                    {
+                        label: 'Destination',
+                        name: 'destination',
+                        options: {
+                            label: value =>
+                                optionsDestination.value.find(option => option.type === value)?.text
+                                ?? null,
+                            options: optionsDestination.value
                         },
-                        {
-                            text: 'Production',
-                            value: 'production'
-                        },
-                        {
-                            text: 'Réception',
-                            value: 'réception'
-                        },
-                        {
-                            text: 'Magasin pièces finies',
-                            value: 'magasin pièces finies'
-                        },
-                        {
-                            text: 'Expédition',
-                            value: 'expédition'
-                        },
-                        {
-                            text: 'Magasin matières premières',
-                            value: 'magasin matières premières'
-                        },
-                        {
-                            text: 'Camion',
-                            value: 'camion'
-                        }
-                    ]
-                },
-                type: 'multiselect'
+                        type: 'select'
+                    }
+                )
             }
-        )
+        } else if (typeof Generalitesfields.find(item => item.name === 'destination') !== 'undefined') {
+            Generalitesfields.pop()
+        }
     }
-
     async function updateGeneral() {
         const form = document.getElementById('addGeneralites')
         const formData = new FormData(form)
         store.setName(formData.get('name'))
         store.setCompany(formData.get('company'))
+        const strFamilies = formData.get('families')
+        store.setFamilies(strFamilies.split(','))
         store.setDestination(formData.get('destination'))
 
         await store.update()
@@ -161,7 +168,8 @@
                         id="addGeneralites"
                         :fields="Generalitesfields"
                         :component-attribute="store.items"
-                        @update="updateGeneral"/>
+                        @update="updateGeneral"
+                        @update:model-value="localDataChange"/>
                 </AppSuspense>
             </AppTab>
             <AppTab id="gui-start-files" title="Fichier" icon="folder" tabs="gui-start">
