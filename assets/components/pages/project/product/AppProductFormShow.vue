785ad44cfe09c51f9ce647f13a97ddb6bbe1c285
marcodec <marc.declercq@laposte.net>
2023-09-26 09:05:36 +0200

    Product::show réorganisation des tabs de la partie Modification des attributs par processus

--- assets/components/pages/project/product/AppProductFormShow.vue
+++ assets/components/pages/project/product/AppProductFormShow.vue
@@ -28,6 +28,28 @@
             <AppSuspense><AppShowProductTabGeneral/></AppSuspense>
         </AppTab>
         <AppTab
+            id="gui-start-files"
+            title="Fichiers"
+            icon="laptop"
+            tabs="gui-start">
+            <AppSuspense>
+                <AppTabFichiers
+                    attachment-element-label="product"
+                    :element-api-url="`/api/products/${idProduct}`"
+                    :element-attachment-store="fetchProductAttachmentStore"
+                    :element-id="idProduct"
+                    element-parameter-name="PRODUCT_ATTACHMENT_CATEGORIES"
+                    :element-store="useProductStore"/>
+            </AppSuspense>
+        </AppTab>
+        <AppTab
+            id="gui-start-Prix"
+            title="Prix"
+            icon="circle-dollar-to-slot"
+            tabs="gui-start">
+            <AppSuspense><AppShowProductTabPrice/></AppSuspense>
+        </AppTab>
+        <AppTab
             id="gui-start-project"
             title="Project"
             icon="pencil"
@@ -54,28 +76,6 @@
             icon="pallet"
             tabs="gui-start">
             <AppSuspense><AppShowProductTabAdmin/></AppSuspense>
-        </AppTab>
-        <AppTab
-            id="gui-start-files"
-            title="Fichiers"
-            icon="laptop"
-            tabs="gui-start">
-            <AppSuspense>
-                <AppTabFichiers
-                    attachment-element-label="product"
-                    :element-api-url="`/api/products/${idProduct}`"
-                    :element-attachment-store="fetchProductAttachmentStore"
-                    :element-id="idProduct"
-                    element-parameter-name="PRODUCT_ATTACHMENT_CATEGORIES"
-                    :element-store="useProductStore"/>
-            </AppSuspense>
-        </AppTab>
-        <AppTab
-            id="gui-start-Prix"
-            title="Prix"
-            icon="circle-dollar-to-slot"
-            tabs="gui-start">
-            <AppSuspense><AppShowProductTabPrice/></AppSuspense>
         </AppTab>
     </AppTabs>
 </template>
