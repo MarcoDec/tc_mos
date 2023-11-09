7cd7295409003a62b341b65688f0646adc878784
marcodec <marc.declercq@laposte.net>
2023-11-09 13:29:53 +0100

    balanceSheetItems.js Fix pb de store commun à plusieurs tableaux différents => Ajout identifiant distinct pour chacun des stores de tableau

--- assets/stores/management/balance-sheets/balanceSheetItems.js
+++ assets/stores/management/balance-sheets/balanceSheetItems.js
@@ -1,35 +1,38 @@
 import {defineStore} from 'pinia'
 import api from '../../../api'
 
-export const useBalanceSheetItemStore = defineStore('balanceSheetsItemStore', {
-    actions: {
-        async add(data) {
-            await api('/api/balance-sheet-items', 'POST', data)
+export default function useBalanceSheetItemStore(base = 'base') {
+    const id = `balanceSheetItems/${base}`
+    return defineStore(id, {
+        actions: {
+            async add(data) {
+                await api('/api/balance-sheet-items', 'POST', data)
+            },
+            async fetch(criteria = '?pagination=false') {
+                const response = await api(`/api/balance-sheet-items${criteria}`, 'GET')
+                this.items = response['hydra:member']
+            },
+            async fetchById(id) {
+                this.item = await api(`/api/balance-sheet-items/${id}`, 'GET')
+            },
+            async update(data, id) {
+                await api(`/api/balance-sheet-items/${id}`, 'PATCH', data)
+            },
+            async remove(id) {
+                await api(`/api/balance-sheet-items/${id}`, 'DELETE')
+            }
         },
-        async fetch(criteria = '?pagination=false') {
-            const response = await api(`/api/balance-sheet-items${criteria}`, 'GET')
-            this.items = response['hydra:member']
+        getters: {
         },
-        async fetchById(id) {
-            this.item = await api(`/api/balance-sheet-items/${id}`, 'GET')
-        },
-        async update(data, id) {
-            await api(`/api/balance-sheet-items/${id}`, 'PATCH', data)
-        },
-        async remove(id) {
-            await api(`/api/balance-sheet-items/${id}`, 'DELETE')
-        }
-    },
-    getters: {
-    },
-    state: () => ({
-        currentPage: '',
-        firstPage: '',
-        lastPage: '',
-        nextPage: '',
-        pagination: false,
-        previousPage: '',
-        items: [],
-        item: null
-    })
-})
+        state: () => ({
+            currentPage: '',
+            firstPage: '',
+            lastPage: '',
+            nextPage: '',
+            pagination: false,
+            previousPage: '',
+            items: [],
+            item: null
+        })
+    })()
+}
