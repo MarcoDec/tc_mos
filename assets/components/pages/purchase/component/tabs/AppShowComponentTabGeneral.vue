eb2c134ce21fc8516d5ffb60ff9aa532f38b8438
marcodec <marc.declercq@laposte.net>
2024-01-10 16:33:20 +0100

    Component:Show Ajout champ famille

--- assets/components/pages/purchase/component/tabs/AppShowComponentTabGeneral.vue
+++ assets/components/pages/purchase/component/tabs/AppShowComponentTabGeneral.vue
@@ -1,15 +1,22 @@
 <script setup>
+    import useOptions from '../../../../../stores/option/options'
     import {useComponentListStore} from '../../../../../stores/purchase/component/components'
     import {useRoute} from 'vue-router'
 
     const route = useRoute()
     const idComponent = route.params.id_component
     const useFetchComponentStore = useComponentListStore()
+    const componentFamilyOptions = useOptions('component-families')
+    await componentFamilyOptions.fetchOp()
     //await useFetchComponentStore.fetchOne(idComponent)
     const Géneralitésfields = [
-        {label: 'Nom', name: 'name', type: 'text'},
-        {label: 'Index', name: 'index', type: 'number'},
-        {label: 'Note', name: 'notes', type: 'textarea'}
+        {label: 'Famille', name: 'family', type: 'select', options: {
+                label: value => componentFamilyOptions.options.find(option => option['@id'] === value)?.text ?? null,
+                options: componentFamilyOptions.options
+            }},
+        {label: 'Indice', name: 'index', type: 'number'},
+        {label: 'Désignation', name: 'name', type: 'text'},
+        {label: 'Notes', name: 'notes', type: 'textarea'}
     ]
     async function updateGeneral() {
         const form = document.getElementById('addGeneralites')
@@ -17,7 +24,8 @@
         const data = {
             index: formData.get('index'),
             name: formData.get('name'),
-            notes: formData.get('notes')
+            notes: formData.get('notes'),
+            family: formData.get('family')
         }
         await useFetchComponentStore.updateAdmin(data, idComponent) //pour index et name
         await useFetchComponentStore.updateMain(data, idComponent) //pour notes
