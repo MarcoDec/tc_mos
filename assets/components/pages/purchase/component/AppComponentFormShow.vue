1d76943bcbe5e1586c2cb45702bc2d8d8912397b
marcodec <marc.declercq@laposte.net>
2024-01-11 14:51:17 +0100

    Component:Show réactivation visualisation Attribut

--- assets/components/pages/purchase/component/AppComponentFormShow.vue
+++ assets/components/pages/purchase/component/AppComponentFormShow.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import AppShowComponentTabAttributes from './tabs/AppShowComponentTabAttributes.vue'
+
     // import AppShowComponentTabAttributes from './tabs/AppShowComponentTabAttributes.vue'
     import AppShowComponentTabFichiers from './tabs/AppShowComponentTabFichiers.vue'
     import AppShowComponentTabGeneral from './tabs/AppShowComponentTabGeneral.vue'
@@ -23,7 +25,7 @@
             title="Attribut"
             icon="sitemap"
             tabs="gui-start">
-            <!-- <AppSuspense><AppShowComponentTabAttributes/></AppSuspense> -->
+            <AppSuspense><AppShowComponentTabAttributes/></AppSuspense>
         </AppTab>
         <AppTab
             id="gui-start-files"
