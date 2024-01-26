f7539eb83fb36f00701db175bbe2070db46ee40d
Marc Declercq <marc.declercq@laposte.net>
2024-01-26 17:10:11 +0100

    Tool:show Adaptation mise en forme similaire à Component:show

--- assets/components/pages/production/equipment/show/tool/AppShowGuiTool.vue
+++ assets/components/pages/production/equipment/show/tool/AppShowGuiTool.vue
@@ -5,13 +5,61 @@
     //import useOptions from '../../../../../stores/option/options'
     import {useRoute} from 'vue-router'
     import {useToolsStore} from '../../../../../../stores/production/engine/tool/tools'
+    import {onBeforeMount, ref} from "vue";
+    import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+    import AppBtn from "../../../../../AppBtn.vue";
+    import AppImg from "../../../../../AppImg.vue";
+    import AppShowComponentTabGeneral from "../../../../purchase/component/show/left/AppShowComponentTabGeneral.vue";
+    import AppComponentShowInlist from "../../../../purchase/component/show/AppComponentShowInlist.vue";
+    import AppComponentFormShow from "../../../../purchase/component/show/AppComponentFormShow.vue";
+    import AppShowToolTabGeneral from "./AppShowToolTabGeneral.vue";
 
     const route = useRoute()
     const idEngine = Number(route.params.id_engine)
+    const beforeMountDataLoaded = ref(false)
+    const keyTitle = ref(0)
+    const keyTabs = ref(0)
+    const modeDetail = ref(true)
+    const isFullScreen = ref(false)
+    const imageUpdateUrl = `/api/engines/${idEngine}/image`
     //region récupération information Outils
     const useFetchToolsStore = useToolsStore()
     useFetchToolsStore.fetchOne(idEngine)
+    onBeforeMount(() => {
+        const promises = []
+        console.log('onBeforeMount')
+        // promises.push(fetchUnits.fetchOp())
+        promises.push(useFetchToolsStore.fetchOne(idEngine))
+        Promise.all(promises).then(() => {
+            beforeMountDataLoaded.value = true
+        })
+    })
+    const requestDetails = () => {
+        modeDetail.value = true
+    }
+    const requestExploitation = () => {
+        modeDetail.value = false
+    }
     //endregion
+    const activateFullScreen = () => {
+        isFullScreen.value = true
+    }
+    const deactivateFullScreen = () => {
+        isFullScreen.value = false
+    }
+    const onUpdated = () => {
+        // console.log('onUpdated')
+        const promises = []
+        useFetchToolsStore.isLoaded = false
+        promises.push(useFetchToolsStore.fetchOne(idEngine))
+        // promises.push(fetchUnits.fetchOp())
+        Promise.all(promises).then(() => {
+            keyTitle.value++
+        })
+    }
+    const onImageUpdate = () => {
+        window.location.reload()
+    }
     // //region récupéation information unité ??
     // const fetchUnits = useOptions('units')
     // fetchUnits.fetchOp()
@@ -20,21 +68,38 @@
 
 <template>
     <AppSuspense>
-        <AppShowGuiGen>
-            <template #gui-header>
-                <div v-if="useFetchToolsStore.isLoaded" class="bg-white border-1 border-dark">
+        <AppShowGuiGen v-if="beforeMountDataLoaded">
+            <template #gui-left>
+                <div :key="`title-${keyTitle}`" class="bg-white border-1 p-1">
+                    <FontAwesomeIcon icon="puzzle-piece"/>
                     <b>{{ useFetchToolsStore.engine.code }}</b>: {{ useFetchToolsStore.engine.name }}
+                    <span class="btn-float-right">
+                        <AppBtn :class="{'selected-detail': modeDetail}" label="Détails" icon="eye" variant="secondary" @click="requestDetails"/>
+                        <AppBtn :class="{'selected-detail': !modeDetail}" label="Exploitation" icon="industry" variant="secondary" @click="requestExploitation"/>
+                    </span>
+                </div>
+                <div class="d-flex flex-row">
+                    <AppImg
+                        class="width30"
+                        :file-path="useFetchToolsStore.engine.filePath"
+                        :image-update-url="imageUpdateUrl"
+                        @update:file-path="onImageUpdate"/>
+                    <AppSuspense><AppShowToolTabGeneral  :key="`form-${keyTabs}`" class="width70" @updated="onUpdated"/></AppSuspense>
                 </div>
             </template>
-            <template #gui-left>
-                <AppSuspense><AppToolFormShow v-if="useFetchToolsStore.isLoaded"/></AppSuspense>
-            </template>
             <template #gui-bottom>
-                <!--            <AppTabs id="gui-bottom">-->
-                <!--                <AppTab id="gui-bottom-components" active icon="puzzle-piece" tabs="gui-bottom" title="Fournitures"/>-->
-                <!--                <AppTab id="gui-bottom-receipts" icon="receipt" tabs="gui-bottom" title="Réceptions"/>-->
-                <!--                <AppTab id="gui-bottom-orders" icon="shopping-cart" tabs="gui-bottom" title="Commandes"/>-->
-                <!--            </AppTabs>-->
+                <div :class="{'full-screen': isFullScreen}" class="bg-warning-subtle font-small">
+                    <div class="full-visible-width">
+                        <AppSuspense>
+                            <AppToolFormShow v-if="modeDetail" :key="`formtab-${keyTabs}`" class="width100"/>
+                            <!--   <AppComponentShowInlist v-else :key="`formlist-${keyTabs}`" class="width100"/>-->
+                        </AppSuspense>
+                    </div>
+                    <span>
+                        <FontAwesomeIcon v-if="isFullScreen" icon="fa-solid fa-magnifying-glass-minus" @click="deactivateFullScreen"/>
+                        <FontAwesomeIcon v-else icon="fa-solid fa-magnifying-glass-plus" @click="activateFullScreen"/>
+                    </span>
+                </div>
             </template>
             <template #gui-right>
                 <!--            {{ route.params.id_product }}-->
