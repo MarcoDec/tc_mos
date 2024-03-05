929bb63c8c29b9d317ca9d28813c39e2f522ec07
Marc Declercq <marc.declercq@laposte.net>
2024-03-05 11:10:20 +0100

    Machine: Ajout relation machine parente

--- src/Entity/Production/Engine/Machine/Machine.php
+++ src/Entity/Production/Engine/Machine/Machine.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Entity\Embeddable\Hr\Employee\Roles;
 use App\Entity\Production\Engine\Engine;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation as Serializer;
 
@@ -51,4 +52,53 @@ class Machine extends Engine {
         Serializer\Groups(['read:engine', 'write:engine'])
     ]
     protected $group;
+
+    #[
+        ApiProperty(description: 'Machine parente', readableLink: false, example: '/api/machines/1'),
+        ORM\ManyToOne(targetEntity: Machine::class, inversedBy: 'subMachines'),
+        Serializer\Groups(['read:engine', 'write:engine'])
+    ]
+    private ?Machine $parentMachine;
+    #[
+        ApiProperty(description: 'Sous-machines', readableLink: false, example: '/api/machines/1'),
+        ORM\OneToMany( mappedBy: 'parentMachine', targetEntity: Machine::class),
+        Serializer\Groups(['read:engine', 'write:engine'])
+    ]
+    private ArrayCollection $subMachines;
+
+    public function __construct() {
+        parent::__construct();
+        $this->subMachines = new ArrayCollection();
+    }
+
+    public function getSubMachines(): ArrayCollection
+    {
+        return $this->subMachines;
+    }
+    public function addSubMachine(Machine $subMachine): self
+    {
+        if (!$this->subMachines->contains($subMachine)) {
+            $this->subMachines[] = $subMachine;
+            $subMachine->setParentMachine($this);
+        }
+        return $this;
+    }
+    public function removeSubMachine(Machine $subMachine): self
+    {
+        if ($this->subMachines->removeElement($subMachine)) {
+            if ($subMachine->getParentMachine() === $this) {
+                $subMachine->setParentMachine(null);
+            }
+        }
+        return $this;
+    }
+    public function getParentMachine(): ?Machine
+    {
+        return $this->parentMachine;
+    }
+    public function setParentMachine(?Machine $parentMachine): self
+    {
+        $this->parentMachine = $parentMachine;
+        return $this;
+    }
 }
