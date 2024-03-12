320947420a7db554f0ec06d2602c3b03e1238655
Marc Declercq <marc.declercq@laposte.net>
2024-03-12 11:55:19 +0100

    PurchaseOrderSubscriber Ajout commentaires / Petites correction logique + Update ReceivedQuantity dans PurchaseOrderItem

--- src/EventSubscriber/PurchaseOrderSubscriber.php
+++ src/EventSubscriber/PurchaseOrderSubscriber.php
@@ -4,13 +4,15 @@ namespace App\EventSubscriber;
 
 use App\Entity\Logistics\Order\Receipt;
 use App\Entity\Purchase\Order\Order as PurchaseOrder;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Workflow\Event\Event;
 use Symfony\Component\Workflow\Registry;
 use Psr\Log\LoggerInterface as Logger;
 
 /**
- * Ce fichier à pour but de gérer les transitions des objets PurchaseOrder et Receipt
+ * Ce fichier à pour but de gérer les transitions des objets PurchaseOrder et
+ * Receipt
  ** PurchaseOrder -> validate -> PurchaseOrderItem
  ** PurchaseOrder -> pay -> PurchaseOrderItem
  ** Receipt -> validate -> PurchaseOrderItem
@@ -26,7 +28,8 @@ class PurchaseOrderSubscriber implements EventSubscriberInterface
     }
 
     /**
-     * Cette méthode permet de gérer les transitions des objets PurchaseOrder "validate" et "pay"
+     * Cette méthode permet de gérer les transitions des objets PurchaseOrder
+     * "validate" et "pay"
      * @param Event $event
      * @return void
      */
@@ -38,23 +41,32 @@ class PurchaseOrderSubscriber implements EventSubscriberInterface
         $transitionName = $transition->getName();
 
         if ($object instanceof PurchaseOrder && $workflowName === 'purchase_order') {
-            if ($transitionName === 'validate') {
-                $this->updateWorkflowState($object->getItems(), 'purchase_order_item', 'validate', $this->workflowRegistry);
+            $po_blocker = $object->getEmbBlocker()->getstate();
+            /**
+             * Si la transition est "validate" et que le blocker est "enabled" alors on applique la transition "validate" à chaque item de la commande
+             */
+            if ($transitionName === 'validate' && $po_blocker === 'enabled') {
+                $this->applyTransitionToItems($object->getItems(), 'purchase_order_item', 'validate', $this->workflowRegistry);
             }
-            if ($transitionName === 'pay') {
+            /**
+             * Si la transition est "pay" et que le blocker est "enabled" alors on applique la transition "pay" à chaque item de la commande
+             */
+            if ($transitionName === 'pay' && $po_blocker === 'enabled') {
                 $items = $object->getItems();
                 foreach ($items as $item) {
                     $blocker = $item->getEmbBlocker()->getstate();
                     $state = $item->getEmbState()->getstate();
-                    if ($blocker === 'enabled') {
-                        if ($state !== 'paid') {
-                            $this->applyTransitionToWorkflow($item, 'purchase_order_item', 'pay', $this->workflowRegistry);
-                        }
+                    /**
+                     * Si le blocker de l'item est "enabled" et que son état est différent de "paid" alors on applique la transition "pay" à l'item
+                     */
+                    if ($blocker === 'enabled' && $state !== 'paid') {
+                        $this->applyTransition($item, 'purchase_order_item', 'pay', $this->workflowRegistry);
                     }
                 }
             }
         }
     }
+
     public function onWorkflowReceiptTransition(Event $event): void
     {
         $object = $event->getSubject();
@@ -63,43 +75,89 @@ class PurchaseOrderSubscriber implements EventSubscriberInterface
         $transitionName = $transition->getName();
 
         if ($object instanceof Receipt && $workflowName === 'receipt') {
-            if ($transitionName === 'validate') {
-            $quantity_confirmed = $object->getItem()->getConfirmedQuantity()->getValue();
-            $quantity_received = $object->getQuantity()->getValue();
-            $state=$object->getItem()->getEmbState()->getState();
-            if($quantity_received >= $quantity_confirmed) {
-                if($state != 'inital' || $state != 'monthly' ) {
-                $this->logger->info(`la commande fournisseur est en êtat $state passe directement a received ` . $state);
-                $this->applyTransitionToWorkflow($object->getItem(), 'purchase_order_item', 'validate', $this->workflowRegistry);
-                $this->applyTransitionToWorkflow($object->getItem(), 'purchase_order_item', 'partially_receive', $this->workflowRegistry);
+            $receipt_blocker = $object->getEmbBlocker()->getstate();
+            $purchaseOrderItem = $object->getItem();
+            /**
+             * Si la transition est "validate" et que le blocker est "enabled" alors on continue
+             */
+            if ($transitionName === 'validate' && $receipt_blocker === 'enabled') {
+                $received_quantity = $object->getQuantity();
+                if (!isset($purchaseOrderItem)) {
+                    $this->logger->warning(
+                        `La réception ${$object->getId()} n'est associé à aucun item de commande`,
+                        ['receipt' => $object, 'workflow' => 'receipt', 'transition' => 'validate', 'blocker' => $receipt_blocker, 'purchaseOrderItem' => $purchaseOrderItem]
+                    );
+                    return;
                 }
-            $this->applyTransitionToWorkflow($object->getItem(), 'purchase_order_item', 'receive', $this->workflowRegistry); 
-            } 
-            if($quantity_received < $quantity_confirmed) {
-                if($state != 'inital' || $state != 'monthly' ) {
-                $this->logger->info(`la commande fournisseur est en êtat $state passe directement a partially_received ` . $state);
-                $this->applyTransitionToWorkflow($object->getItem(), 'purchase_order_item', 'validate', $this->workflowRegistry);
-            } 
-            $this->applyTransitionToWorkflow($object->getItem(), 'purchase_order_item', 'partially_receive', $this->workflowRegistry);
+                $purchaseOrderItemState = $purchaseOrderItem->getEmbState()->getState();
+                $purchased_quantity_confirmed = $purchaseOrderItem->getConfirmedQuantity();
+                $purchased_received_quantity = $purchaseOrderItem->getReceivedQuantity();
 
-        }          
+                // On ajoute la quantité reçue à la quantité déjà reçue
+                $purchased_received_quantity->add($received_quantity);
 
+                if ($purchased_received_quantity->isGreaterThanOrEqual($purchased_quantity_confirmed)) {
+                    /**
+                     * Si la quantité reçue est supérieure ou égale à la quantité confirmée
+                     * et que l'état de l'item est "agreed"
+                     * alors on applique la transition "receive" à l'item de commande
+                     */
+                    if ($purchaseOrderItemState === 'agreed' || $purchaseOrderItemState === 'partially_received') {
+                        $this->applyTransition($purchaseOrderItem, 'purchase_order_item', 'receive', $this->workflowRegistry);
+                    } else {
+                        $this->logger->warning(
+                            `La réception ${$object->getId()} est associé à l'item de commande ${$purchaseOrderItem->getId()} dans le statut non compatible $purchaseOrderItemState`,
+                            ['receipt' => $object, 'workflow' => 'receipt', 'transition' => 'validate', 'blocker' => $receipt_blocker, 'purchaseOrderItem' => $purchaseOrderItem]
+                        );
+                    }
+                } else {
+                    /**
+                     * Si la quantité reçue est inférieure à la quantité confirmée
+                     * et que l'état de l'item est "agreed"
+                     * alors on applique la transition "partially_receive" à l'item de commande
+                     */
+                    if ($purchaseOrderItemState === 'agreed') {
+                        $this->applyTransition($purchaseOrderItem, 'purchase_order_item', 'partially_receive', $this->workflowRegistry);
+                    } else {
+                        if ($purchaseOrderItemState !== 'partially_received') {
+                            $this->logger->warning(
+                                `La réception ${$object->getId()} est associé à l'item de commande ${$purchaseOrderItem->getId()} dans le statut non compatible $purchaseOrderItemState`,
+                                ['receipt' => $object, 'workflow' => 'receipt', 'transition' => 'validate', 'blocker' => $receipt_blocker, 'purchaseOrderItem' => $purchaseOrderItem]
+                            );
+                        }
+                    }
+                }
             }
         }
     }
 
-    public function updateWorkflowState($objects, $workflowName, $transitionName, $workflowRegistry): void
+
+    /**
+     * Cette méthode permet de mettre à jour l'état d'une collection d'objets
+     * @param ArrayCollection $objects
+     * @param string          $workflowName
+     * @param string          $transitionName
+     * @param Registry        $workflowRegistry
+     * @return void
+     */
+    public
+    function applyTransitionToItems(ArrayCollection $objects, string $workflowName, string $transitionName, Registry $workflowRegistry): void
     {
         foreach ($objects as $object) {
-            $this->applyTransitionToWorkflow($object, $workflowName, $transitionName, $workflowRegistry);
-//            $workflow = $workflowRegistry->get($object, $workflowName);
-//            if ($workflow->can($object, $transitionName)) {
-//                $workflow->apply($object, $transitionName);
-//            }
+            $this->applyTransition($object, $workflowName, $transitionName, $workflowRegistry);
         }
     }
 
-    public function applyTransitionToWorkflow($object, $workflowName, $transitionName, $workflowRegistry): void
+    /**
+     * Cette méthode permet d'appliquer une transition à un objet
+     * @param $object
+     * @param $workflowName
+     * @param $transitionName
+     * @param $workflowRegistry
+     * @return void
+     */
+    public
+    function applyTransition($object, $workflowName, $transitionName, $workflowRegistry): void
     {
         $workflow = $workflowRegistry->get($object, $workflowName);
 
@@ -108,7 +166,8 @@ class PurchaseOrderSubscriber implements EventSubscriberInterface
         }
     }
 
-    public static function getSubscribedEvents(): array
+    public
+    static function getSubscribedEvents(): array
     {
         return [
             'workflow.purchase_order.transition' => 'onWorkflowPurchaseOrderTransition',
