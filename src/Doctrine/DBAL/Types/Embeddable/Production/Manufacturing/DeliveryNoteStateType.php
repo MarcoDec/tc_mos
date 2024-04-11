1f94ee172a293d8bb22d6f2d2834c2f561dcd56f
marcodec <marc.declercq@laposte.net>
2024-04-11 16:06:16 +0200

    Nettoyage et amélioration lecture States

--- src/Doctrine/DBAL/Types/Embeddable/Production/Manufacturing/DeliveryNoteStateType.php
+++ src/Doctrine/DBAL/Types/Embeddable/Production/Manufacturing/DeliveryNoteStateType.php
@@ -5,7 +5,11 @@ namespace App\Doctrine\DBAL\Types\Embeddable\Production\Manufacturing;
 use App\Doctrine\DBAL\Types\Embeddable\StateType;
 
 final class DeliveryNoteStateType extends StateType {
-    final public const TYPES = [self::TYPE_STATE_DRAFT, self::TYPE_STATE_SENT, self::TYPE_STATE_ACKNOWLEDGMENT_OF_RECEIPT ,self::TYPE_STATE_REJECTED];
+    final public const TYPES = [
+        self::TYPE_STATE_DRAFT,
+        self::TYPE_STATE_SENT,
+        self::TYPE_STATE_ACKNOWLEDGMENT_OF_RECEIPT ,
+        self::TYPE_STATE_REJECTED];
 
     public function getName(): string {
         return 'delivery_note_state';
