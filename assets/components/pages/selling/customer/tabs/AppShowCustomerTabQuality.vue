780192c8f24b8525c66e975ddec7bdf070ae24f2
Marc Declercq <marc.declercq@laposte.net>
2024-05-13 15:23:26 +0200

    Customer:show Quality// Séparation champs spécifique au portail

--- assets/components/pages/selling/customer/tabs/AppShowCustomerTabQuality.vue
+++ assets/components/pages/selling/customer/tabs/AppShowCustomerTabQuality.vue
@@ -11,13 +11,19 @@
     const fetchCustomerStore = useCustomerStore()
     const localData = ref({})
     localData.value = {
-        getPassword: props.dataCustomers.qualityPortal.password,
-        getUrl: props.dataCustomers.qualityPortal.url,
-        getUsername: props.dataCustomers.qualityPortal.username,
         ppmRate: props.dataSociety.ppmRate
     }
     const qualityFields = [
-        {label: 'Nb PPM', name: 'ppmRate', type: 'number'},
+        {label: 'Nb PPM', name: 'ppmRate', type: 'number'}
+    ]
+
+    const localQualityPortalData = ref({})
+    localQualityPortalData.value = {
+        getUrl: props.dataCustomers.qualityPortal.url,
+        getUsername: props.dataCustomers.qualityPortal.username,
+        getPassword: props.dataCustomers.qualityPortal.password
+    }
+    const qualityPortalFields = [
         {label: 'Url *', name: 'getUrl', type: 'text'},
         {label: 'Ident', name: 'getUsername', type: 'text'},
         {label: 'Password', name: 'getPassword', type: 'text'}
@@ -26,6 +32,11 @@
         const data = {
             ppmRate: localData.value.ppmRate
         }
+        const itemSoc = generateSocieties(props.dataSociety)
+        await itemSoc.update(data)
+        await fetchCustomerStore.fetchOne(props.dataCustomers.id)
+    }
+    async function updateQualityPortalFields() {
         const dataQuality = {
             qualityPortal: {
                 password: localData.value.getPassword,
@@ -35,20 +46,46 @@
         }
         const item = generateCustomer(props.dataCustomers)
         await item.updateQuality(dataQuality)
-        const itemSoc = generateSocieties(props.dataSociety)
-        await itemSoc.update(data)
         await fetchCustomerStore.fetchOne(props.dataCustomers.id)
     }
     function updateLocalData(value) {
         localData.value = value
     }
+    function updateLocalQualityPortalData(value) {
+        localQualityPortalData.value = value
+    }
 </script>
 
 <template>
-    <AppCardShow
-        id="addQualite"
-        :fields="qualityFields"
-        :component-attribute="localData"
-        @update="updateQualityFields"
-        @update:model-value="updateLocalData"/>
+    <div class="tabQuality">
+        <AppCardShow
+            id="addQualite"
+            class="qualityCard"
+            :fields="qualityFields"
+            :component-attribute="localData"
+            title="Paramètres Qualité"
+            @update="updateQualityFields"
+            @update:model-value="updateLocalData"/>
+        <AppCardShow
+            id="addPortalQualite"
+            class="qualityCard"
+            title="Portail Qualité"
+            :fields="qualityPortalFields"
+            :component-attribute="localQualityPortalData"
+            @update="updateQualityPortalFields"
+            @update:model-value="updateLocalQualityPortalData"/>
+    </div>
 </template>
+
+<style scoped>
+    div.tabQuality {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap
+    }
+    .qualityCard {
+        min-width: 500px;
+        margin-left: 20px;
+        margin-bottom: 20px;
+    }
+</style>
